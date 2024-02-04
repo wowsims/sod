6cdb05273a842e3ce430c89b1435b4dd072e26e2
The Grox Empire <47816228+TheGroxEmpire@users.noreply.github.com>
2024-02-04 07:23:30 +0700

    Warrior: Update known issue

--- ui/warrior/sim.ts
+++ ui/warrior/sim.ts
@@ -28,7 +28,8 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecWarrior, {
 	// List any known bugs / issues here and they'll be shown on the site.
 	knownIssues: [
 		"Rage conversion is a bit off for non 60s",
-		"Prot oriented Rune: Devastate and Sword and Board is not implemented"
+		"Prot oriented Rune: Devastate and Sword and Board is not yet implemented",
+		"Head and Bracers Rune is not yet implemented"
 	],
 
 	// All stats for which EP should be calculated.
