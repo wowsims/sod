1130b03a1e5b7969c7e8d66bb7c7dd36355e0da0
rosenrusinov <rainisunbelievable@gmail.com>
2024-02-05 23:34:50 +0100

    imp max firebolt rank

--- ui/warlock/inputs.ts
+++ ui/warlock/inputs.ts
@@ -1,7 +1,8 @@
 import {
 	WarlockOptions_Armor as Armor,
 	WarlockOptions_Summon as Summon,
-	WarlockOptions_WeaponImbue as WeaponImbue
+	WarlockOptions_WeaponImbue as WeaponImbue,
+	WarlockOptions_MaxFireboltRank as MaxFireboltRank
 } from '../core/proto/warlock.js';
 
 import { Player } from '../core/player.js';
@@ -39,6 +40,22 @@ export const PetInput = InputHelpers.makeSpecOptionsEnumIconInput<Spec.SpecWarlo
 		{ actionId: () => ActionId.fromSpellId(697), value: Summon.Voidwalker },
 		{ actionId: () => ActionId.fromSpellId(712), value: Summon.Succubus },
 		{ actionId: () => ActionId.fromSpellId(691), value: Summon.Felhunter },
+	],
+	changeEmitter: (player: Player<Spec.SpecWarlock>) => player.changeEmitter,
+});
+
+export const ImpFireboltRank = InputHelpers.makeSpecOptionsEnumIconInput<Spec.SpecWarlock, MaxFireboltRank>({
+	fieldName: 'maxFireboltRank',
+	showWhen: (player) => player.getSpecOptions().summon == Summon.Imp,
+	values: [
+		{ value: MaxFireboltRank.NoMaximum, tooltip: 'Max' },
+		{ actionId: () => ActionId.fromSpellId(3110), value: MaxFireboltRank.Rank1 },
+		{ actionId: () => ActionId.fromSpellId(7799), value: MaxFireboltRank.Rank2 },
+		{ actionId: () => ActionId.fromSpellId(7800), value: MaxFireboltRank.Rank3 },
+		{ actionId: () => ActionId.fromSpellId(7801), value: MaxFireboltRank.Rank4 },
+		{ actionId: () => ActionId.fromSpellId(7802), value: MaxFireboltRank.Rank5 },
+		{ actionId: () => ActionId.fromSpellId(11762), value: MaxFireboltRank.Rank6 },
+		{ actionId: () => ActionId.fromSpellId(11763), value: MaxFireboltRank.Rank7 },
 	],
 	changeEmitter: (player: Player<Spec.SpecWarlock>) => player.changeEmitter,
 });
