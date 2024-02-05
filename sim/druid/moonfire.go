4744b74bda90167c188572d920c154e996faf1d6
Kayla Glick <kaylaglick12@gmail.com>
2024-02-05 11:29:04 -0500

    fix balance spell ranks

--- sim/druid/moonfire.go
+++ sim/druid/moonfire.go
@@ -6,14 +6,37 @@ import (
 	"github.com/wowsims/sod/sim/core"
 )
 
+const MoonfireRanks = 10
+
+var MoonfireSpellId = [MoonfireRanks + 1]int32{0, 8921, 8924, 8925, 8926, 8927, 8928, 8929, 9833, 9834, 9835}
+var MoonfiresSpellCoeff = [MoonfireRanks + 1]float64{0, .06, .094, .128, .15, .15, .15, .15, .15, .15, .15}
+var MoonfiresSellDotCoeff = [MoonfireRanks + 1]float64{0, .052, .081, .111, .13, .13, .13, .13, .13, .13, .13}
+var MoonfireBaseDamage = [MoonfireRanks + 1][]float64{{0}, {9, 12}, {17, 21}, {30, 37}, {47, 55}, {70, 82}, {91, 108}, {117, 137}, {143, 168}, {172, 200}, {195, 228}}
+var MoonfireBaseDotDamage = [MoonfireRanks + 1]float64{0, 12, 32, 52, 80, 124, 164, 212, 264, 320, 384}
+var MoonfireManaCost = [MoonfireRanks + 1]float64{0, 25, 50, 75, 105, 150, 190, 235, 280, 325, 375}
+var MoonfireLevel = [MoonfireRanks + 1]int{0, 4, 10, 16, 22, 28, 34, 40, 46, 52, 58}
+
+func (druid *Druid) registerMoonfireSpell() {
+	druid.Moonfire = make([]*DruidSpell, MoonfireRanks+1)
+
+	for rank := 1; rank <= MoonfireRanks; rank++ {
+		config := druid.getMoonfireBaseConfig(rank)
+
+		if config.RequiredLevel <= int(druid.Level) {
+			druid.Moonfire[rank] = druid.RegisterSpell(Humanoid|Moonkin, config)
+		}
+	}
+}
+
 func (druid *Druid) getMoonfireBaseConfig(rank int) core.SpellConfig {
-	spellCoeff := [11]float64{0, .06, .094, .128, .15, .15, .15, .15, .15, .15, .15}[rank]
-	spellDotCoeff := [11]float64{0, .052, .081, .111, .13, .13, .13, .13, .13, .13, .13}[rank]
-	baseDamage := [11][]float64{{0}, {9, 12}, {17, 21}, {30, 37}, {47, 55}, {70, 82}, {91, 108}, {117, 137}, {143, 168}, {172, 200}, {195, 228}}[rank]
-	baseDotDamage := [11]float64{0, 12, 32, 52, 80, 124, 164, 212, 264, 320, 384}[rank]
-	spellId := [11]int32{0, 8921, 8924, 8925, 8926, 8927, 8928, 8929, 9833, 9834, 9835}[rank]
-	manaCost := [11]float64{0, 25, 50, 75, 105, 150, 190, 235, 280, 325, 375}[rank]
-	level := [11]int{0, 4, 10, 16, 22, 28, 34, 40, 46, 52, 58}[rank]
+	spellId := MoonfireSpellId[rank]
+	spellCoeff := MoonfiresSpellCoeff[rank]
+	spellDotCoeff := MoonfiresSellDotCoeff[rank]
+	baseDamageLow := MoonfireBaseDamage[rank][0]
+	baseDamageHigh := MoonfireBaseDamage[rank][0]
+	baseDotDamage := MoonfireBaseDotDamage[rank]
+	manaCost := MoonfireManaCost[rank]
+	level := MoonfireLevel[rank]
 
 	ticks := core.TernaryInt32(rank < 2, 3, 4)
 	impMf := float64(druid.Talents.ImprovedMoonfire)
@@ -58,24 +81,12 @@ func (druid *Druid) getMoonfireBaseConfig(rank int) core.SpellConfig {
 		ThreatMultiplier: 1,
 
 		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-			baseDamage := sim.Roll(baseDamage[0], baseDamage[1]) + spellCoeff*spell.SpellPower()
+			baseDamage := sim.Roll(baseDamageLow, baseDamageHigh) + spellCoeff*spell.SpellPower()
 			result := spell.CalcAndDealDamage(sim, target, baseDamage, spell.OutcomeMagicHitAndCrit)
 
 			if result.Landed() {
 				spell.Dot(target).Apply(sim)
 			}
 		},
-	}
-}
-
-func (druid *Druid) registerMoonfireSpell() {
-	maxRank := 10
-
-	for i := 1; i <= maxRank; i++ {
-		config := druid.getMoonfireBaseConfig(i)
-
-		if config.RequiredLevel <= int(druid.Level) {
-			druid.Moonfire = druid.RegisterSpell(Humanoid|Moonkin, config)
-		}
 	}
 }
--- sim/druid/wrath.go
+++ sim/druid/wrath.go
@@ -6,13 +6,35 @@ import (
 	"github.com/wowsims/sod/sim/core"
 )
 
-func (druid *Druid) getWrathBaseConfig(rank int) core.SpellConfig {
-	spellCoeff := [9]float64{0, 0.123, 0.231, 0.443, 0.571, 0.571, 0.571, 0.571, 0.571}[rank]
-	baseDamage := [9][]float64{{0}, {13, 16}, {28, 33}, {48, 57}, {69, 79}, {108, 123}, {148, 167}, {198, 221}, {248, 277}}[rank]
-	spellId := [9]int32{0, 5176, 5177, 5178, 5179, 5180, 6780, 8905, 9912}[rank]
-	manaCost := [9]float64{0, 20, 35, 55, 70, 100, 125, 155, 180}[rank]
-	level := [9]int{0, 1, 6, 14, 22, 30, 38, 46, 54}[rank]
-	castTime := [9]int{0, 1500, 1700, 2000, 2000, 2000, 2000, 2000, 2000}[rank]
+const WrathRanks = 8
+
+var WrathSpellId = [WrathRanks + 1]int32{0, 5176, 5177, 5178, 5179, 5180, 6780, 8905, 9912}
+var WrathBaseDamage = [WrathRanks + 1][]float64{{0}, {13, 16}, {28, 33}, {48, 57}, {69, 79}, {108, 123}, {148, 167}, {198, 221}, {248, 277}}
+var WrathSpellCoeff = [WrathRanks + 1]float64{0, 0.123, 0.231, 0.443, 0.571, 0.571, 0.571, 0.571, 0.571}
+var WrathManaCost = [WrathRanks + 1]float64{0, 20, 35, 55, 70, 100, 125, 155, 180}
+var WrathCastTime = [WrathRanks + 1]int{0, 1500, 1700, 2000, 2000, 2000, 2000, 2000, 2000}
+var WrathLevel = [WrathRanks + 1]int{0, 1, 6, 14, 22, 30, 38, 46, 54}
+
+func (druid *Druid) registerWrathSpell() {
+	druid.Wrath = make([]*DruidSpell, WrathRanks+1)
+
+	for rank := 1; rank <= WrathRanks; rank++ {
+		config := druid.newWrathSpellConfig(rank)
+
+		if config.RequiredLevel <= int(druid.Level) {
+			druid.Wrath[rank] = druid.RegisterSpell(Humanoid|Moonkin, config)
+		}
+	}
+}
+
+func (druid *Druid) newWrathSpellConfig(rank int) core.SpellConfig {
+	spellId := WrathSpellId[rank]
+	baseDamageLow := WrathBaseDamage[rank][0]
+	baseDamageHigh := WrathBaseDamage[rank][1]
+	spellCoeff := WrathSpellCoeff[rank]
+	manaCost := WrathManaCost[rank]
+	castTime := WrathCastTime[rank]
+	level := WrathLevel[rank]
 
 	return core.SpellConfig{
 		ActionID:      core.ActionID{SpellID: spellId},
@@ -40,24 +62,12 @@ func (druid *Druid) getWrathBaseConfig(rank int) core.SpellConfig {
 		ThreatMultiplier: 1,
 
 		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-			baseDamage := sim.Roll(baseDamage[0], baseDamage[1]) + spellCoeff*spell.SpellPower()
+			baseDamage := sim.Roll(baseDamageLow, baseDamageHigh) + spellCoeff*spell.SpellPower()
 			result := spell.CalcDamage(sim, target, baseDamage, spell.OutcomeMagicHitAndCrit)
 
 			spell.WaitTravelTime(sim, func(sim *core.Simulation) {
 				spell.DealDamage(sim, result)
 			})
 		},
-	}
-}
-
-func (druid *Druid) registerWrathSpell() {
-	maxRank := 8
-
-	for i := 1; i <= maxRank; i++ {
-		config := druid.getWrathBaseConfig(i)
-
-		if config.RequiredLevel <= int(druid.Level) {
-			druid.Wrath = druid.RegisterSpell(Humanoid|Moonkin, config)
-		}
 	}
 }
