0dd2ef0e89799b79802b90a19095c24c7625e5f6
Kayla Glick <kaylaglick12@gmail.com>
2024-02-12 12:02:32 -0500

    disable later phase enchant effects

--- sim/common/sod/enchant_effects.go
+++ sim/common/sod/enchant_effects.go
@@ -1,11 +1,8 @@
 package sod
 
 import (
-	"time"
-
 	"github.com/wowsims/sod/sim/core"
 	"github.com/wowsims/sod/sim/core/proto"
-	"github.com/wowsims/sod/sim/core/stats"
 )
 
 func init() {
@@ -55,45 +52,45 @@ func init() {
 	})
 
 	// Fiery Weapon
-	core.NewEnchantEffect(803, func(agent core.Agent) {
-		character := agent.GetCharacter()
+	// core.NewEnchantEffect(803, func(agent core.Agent) {
+	// 	character := agent.GetCharacter()
 
-		procMask := character.GetProcMaskForEnchant(803)
-		ppmm := character.AutoAttacks.NewPPMManager(6.0, procMask)
+	// 	procMask := character.GetProcMaskForEnchant(803)
+	// 	ppmm := character.AutoAttacks.NewPPMManager(6.0, procMask)
 
-		procSpell := character.RegisterSpell(core.SpellConfig{
-			ActionID:    core.ActionID{SpellID: 13898},
-			SpellSchool: core.SpellSchoolFire,
-			ProcMask:    core.ProcMaskSpellDamage,
+	// 	procSpell := character.RegisterSpell(core.SpellConfig{
+	// 		ActionID:    core.ActionID{SpellID: 13898},
+	// 		SpellSchool: core.SpellSchoolFire,
+	// 		ProcMask:    core.ProcMaskSpellDamage,
 
-			DamageMultiplier: 1,
-			CritMultiplier:   character.DefaultSpellCritMultiplier(),
-			ThreatMultiplier: 1,
+	// 		DamageMultiplier: 1,
+	// 		CritMultiplier:   character.DefaultSpellCritMultiplier(),
+	// 		ThreatMultiplier: 1,
 
-			ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-				spell.CalcAndDealDamage(sim, target, 40, spell.OutcomeMagicHitAndCrit)
-			},
-		})
+	// 		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+	// 			spell.CalcAndDealDamage(sim, target, 40, spell.OutcomeMagicHitAndCrit)
+	// 		},
+	// 	})
 
-		aura := character.GetOrRegisterAura(core.Aura{
-			Label:    "Fiery Weapon",
-			Duration: core.NeverExpires,
-			OnReset: func(aura *core.Aura, sim *core.Simulation) {
-				aura.Activate(sim)
-			},
-			OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
-				if !result.Landed() {
-					return
-				}
+	// 	aura := character.GetOrRegisterAura(core.Aura{
+	// 		Label:    "Fiery Weapon",
+	// 		Duration: core.NeverExpires,
+	// 		OnReset: func(aura *core.Aura, sim *core.Simulation) {
+	// 			aura.Activate(sim)
+	// 		},
+	// 		OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+	// 			if !result.Landed() {
+	// 				return
+	// 			}
 
-				if ppmm.Proc(sim, spell.ProcMask, "Fiery Weapon") {
-					procSpell.Cast(sim, result.Target)
-				}
-			},
-		})
+	// 			if ppmm.Proc(sim, spell.ProcMask, "Fiery Weapon") {
+	// 				procSpell.Cast(sim, result.Target)
+	// 			}
+	// 		},
+	// 	})
 
-		character.ItemSwap.RegisterOnSwapItemForEffectWithPPMManager(803, 6.0, &ppmm, aura)
-	})
+	// 	character.ItemSwap.RegisterOnSwapItemForEffectWithPPMManager(803, 6.0, &ppmm, aura)
+	// })
 
 	// Minor Striking
 	core.AddWeaponEffect(250, func(agent core.Agent, slot proto.ItemSlot) {
@@ -126,63 +123,63 @@ func init() {
 	})
 
 	// Greater Striking
-	core.AddWeaponEffect(805, func(agent core.Agent, slot proto.ItemSlot) {
-		w := agent.GetCharacter().AutoAttacks.MH()
-		if slot == proto.ItemSlot_ItemSlotOffHand {
-			w = agent.GetCharacter().AutoAttacks.OH()
-		}
-		w.BaseDamageMin += 4
-		w.BaseDamageMax += 4
-	})
+	// core.AddWeaponEffect(805, func(agent core.Agent, slot proto.ItemSlot) {
+	// 	w := agent.GetCharacter().AutoAttacks.MH()
+	// 	if slot == proto.ItemSlot_ItemSlotOffHand {
+	// 		w = agent.GetCharacter().AutoAttacks.OH()
+	// 	}
+	// 	w.BaseDamageMin += 4
+	// 	w.BaseDamageMax += 4
+	// })
 
 	// Superior Striking
-	core.AddWeaponEffect(1897, func(agent core.Agent, slot proto.ItemSlot) {
-		w := agent.GetCharacter().AutoAttacks.MH()
-		if slot == proto.ItemSlot_ItemSlotOffHand {
-			w = agent.GetCharacter().AutoAttacks.OH()
-		}
-		w.BaseDamageMin += 5
-		w.BaseDamageMax += 5
-	})
+	// core.AddWeaponEffect(1897, func(agent core.Agent, slot proto.ItemSlot) {
+	// 	w := agent.GetCharacter().AutoAttacks.MH()
+	// 	if slot == proto.ItemSlot_ItemSlotOffHand {
+	// 		w = agent.GetCharacter().AutoAttacks.OH()
+	// 	}
+	// 	w.BaseDamageMin += 5
+	// 	w.BaseDamageMax += 5
+	// })
 
 	// TODO: Crusader, Mongoose, and Executioner could also be modelled as AddWeaponEffect instead
 	// ApplyCrusaderEffect will be applied twice if there is two weapons with this enchant.
 	//   However, it will automatically overwrite one of them, so it should be ok.
 	//   A single application of the aura will handle both mh and oh procs.
-	core.NewEnchantEffect(1900, func(agent core.Agent) {
-		character := agent.GetCharacter()
+	// core.NewEnchantEffect(1900, func(agent core.Agent) {
+	// 	character := agent.GetCharacter()
 
-		procMask := character.GetProcMaskForEnchant(1900)
-		ppmm := character.AutoAttacks.NewPPMManager(1.0, procMask)
+	// 	procMask := character.GetProcMaskForEnchant(1900)
+	// 	ppmm := character.AutoAttacks.NewPPMManager(1.0, procMask)
 
-		// -4 str per level over 60
-		strBonus := 100.0 - 4.0*float64(agent.GetCharacter().Level /*core.CharacterLevel*/ -60)
-		mhAura := character.NewTemporaryStatsAura("Crusader Enchant MH", core.ActionID{SpellID: 20007, Tag: 1}, stats.Stats{stats.Strength: strBonus}, time.Second*15)
-		ohAura := character.NewTemporaryStatsAura("Crusader Enchant OH", core.ActionID{SpellID: 20007, Tag: 2}, stats.Stats{stats.Strength: strBonus}, time.Second*15)
+	// 	// -4 str per level over 60
+	// 	strBonus := 100.0 - 4.0*float64(agent.GetCharacter().Level /*core.CharacterLevel*/ -60)
+	// 	mhAura := character.NewTemporaryStatsAura("Crusader Enchant MH", core.ActionID{SpellID: 20007, Tag: 1}, stats.Stats{stats.Strength: strBonus}, time.Second*15)
+	// 	ohAura := character.NewTemporaryStatsAura("Crusader Enchant OH", core.ActionID{SpellID: 20007, Tag: 2}, stats.Stats{stats.Strength: strBonus}, time.Second*15)
 
-		aura := character.GetOrRegisterAura(core.Aura{
-			Label:    "Crusader Enchant",
-			Duration: core.NeverExpires,
-			OnReset: func(aura *core.Aura, sim *core.Simulation) {
-				aura.Activate(sim)
-			},
-			OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
-				if !result.Landed() {
-					return
-				}
+	// 	aura := character.GetOrRegisterAura(core.Aura{
+	// 		Label:    "Crusader Enchant",
+	// 		Duration: core.NeverExpires,
+	// 		OnReset: func(aura *core.Aura, sim *core.Simulation) {
+	// 			aura.Activate(sim)
+	// 		},
+	// 		OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+	// 			if !result.Landed() {
+	// 				return
+	// 			}
 
-				if ppmm.Proc(sim, spell.ProcMask, "Crusader") {
-					if spell.IsMH() {
-						mhAura.Activate(sim)
-					} else {
-						ohAura.Activate(sim)
-					}
-				}
-			},
-		})
+	// 			if ppmm.Proc(sim, spell.ProcMask, "Crusader") {
+	// 				if spell.IsMH() {
+	// 					mhAura.Activate(sim)
+	// 				} else {
+	// 					ohAura.Activate(sim)
+	// 				}
+	// 			}
+	// 		},
+	// 	})
 
-		character.ItemSwap.RegisterOnSwapItemForEffectWithPPMManager(1900, 1.0, &ppmm, aura)
-	})
+	// 	character.ItemSwap.RegisterOnSwapItemForEffectWithPPMManager(1900, 1.0, &ppmm, aura)
+	// })
 
 	// Weapon - Dismantle
 	core.NewEnchantEffect(7210, func(agent core.Agent) {
@@ -231,16 +228,16 @@ func init() {
 	})
 
 	// Cloak - Subtlety
-	core.NewEnchantEffect(2621, func(agent core.Agent) {
-		character := agent.GetCharacter()
-		character.PseudoStats.ThreatMultiplier *= 0.98
-	})
+	// core.NewEnchantEffect(2621, func(agent core.Agent) {
+	// 	character := agent.GetCharacter()
+	// 	character.PseudoStats.ThreatMultiplier *= 0.98
+	// })
 
 	// Globes - Threat
-	core.NewEnchantEffect(2613, func(agent core.Agent) {
-		character := agent.GetCharacter()
-		character.PseudoStats.ThreatMultiplier *= 1.02
-	})
+	// core.NewEnchantEffect(2613, func(agent core.Agent) {
+	// 	character := agent.GetCharacter()
+	// 	character.PseudoStats.ThreatMultiplier *= 1.02
+	// })
 
 	// Ranged Scopes
 	core.AddWeaponEffect(32, func(agent core.Agent, _ proto.ItemSlot) {
