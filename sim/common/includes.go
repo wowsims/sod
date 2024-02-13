1fa13194c8ce90a66488a0872efbf65ad4b99859
Kayla Glick <kaylaglick12@gmail.com>
2024-02-13 02:09:08 -0500

    load crafted gear

--- sim/common/includes.go
+++ sim/common/includes.go
@@ -3,4 +3,5 @@ package common
 // Just import other directories, so importing common from elsewhere is enough.
 import (
 	_ "github.com/wowsims/sod/sim/common/sod"
+	_ "github.com/wowsims/sod/sim/common/sod/crafted"
 )
