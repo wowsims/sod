4e6409d10be01681a58df472e621e5b566367c44
Kayla Glick <kaylaglick12@gmail.com>
2024-02-18 12:29:42 -0500

    add phase 1 totem/idol as well

--- sim/druid/items.go
+++ sim/druid/items.go
@@ -1,9 +1,19 @@
 package druid
 
+import (
+	"github.com/wowsims/sod/sim/core"
+	"github.com/wowsims/sod/sim/core/stats"
+)
+
 // Totem Item IDs
 const (
-	IdolOfWrath = 216490
+	IdolMindExpandingMushroom = 209576
+	IdolOfWrath               = 216490
 )
 
 func init() {
+	core.NewItemEffect(IdolMindExpandingMushroom, func(agent core.Agent) {
+		character := agent.GetCharacter()
+		character.AddStat(stats.Spirit, 5)
+	})
 }
