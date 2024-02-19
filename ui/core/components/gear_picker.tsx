349a95c90da9807bf0d12ac5543550ff5c00bd6a
Kayla Glick <kaylaglick12@gmail.com>
2024-02-19 13:42:11 -0500

    add comma

--- ui/core/components/gear_picker.tsx
+++ ui/core/components/gear_picker.tsx
@@ -427,7 +427,7 @@ export class SelectorModal extends BaseModal {
 			<div className="d-flex align-items-center form-text mt-3">
 				<i className="fas fa-circle-exclamation fa-xl me-2"></i>
 				<span>
-					If gear is missing check your gear filters and your level in the "Settings" tab.
+					If gear is missing, check your gear filters and your level in the "Settings" tab.
 					<br />
 					If the problem persists, save any un-saved data, click the
 					<i className="fas fa-cog mx-1"></i>
