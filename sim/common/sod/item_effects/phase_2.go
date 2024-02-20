942b7166f9b5b9b3d65b274faab1ca069c1bd904
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-02-20 17:40:44 +0100

    Implement Bloodbark Crusher on use

--- sim/common/sod/item_effects/phase_2.go
+++ sim/common/sod/item_effects/phase_2.go
@@ -79,5 +79,68 @@ func init() {
 		})
 	})
 
+	// Bloodbark Crusher
+	core.NewItemEffect(216499, func(agent core.Agent) {
+		character := agent.GetCharacter()
+		auraActionID := core.ActionID{SpellID: 436482}
+		numHits := min(3, character.Env.GetNumTargets())
+
+		triggeredDmgSpell := character.RegisterSpell(core.SpellConfig{
+			ActionID:    core.ActionID{SpellID: 436481},
+			SpellSchool: core.SpellSchoolNature,
+			ProcMask:    core.ProcMaskEmpty,
+
+			// TODO: "Causes additional threat" in Tooltip, no clue what the multiplier is.
+			ThreatMultiplier: 1,
+			DamageMultiplier: 1,
+			CritMultiplier:   character.DefaultMeleeCritMultiplier(),
+
+			ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+				curTarget := target
+				for hitIndex := int32(0); hitIndex < numHits; hitIndex++ {
+					// The spell is affected by phys school mods because it's nature + physical school.
+					dmgWithPhysMods := 5 * character.PseudoStats.SchoolDamageDealtMultiplier[stats.SchoolIndexPhysical]
+					spell.CalcAndDealDamage(sim, curTarget, dmgWithPhysMods, spell.OutcomeMagicHitAndCrit)
+					curTarget = sim.Environment.NextTargetUnit(curTarget)
+				}
+			},
+		})
+
+		mainAura := character.GetOrRegisterAura(core.Aura{
+			Label:    "Bloodbark Cleave",
+			ActionID: auraActionID,
+			Duration: 20 * time.Second,
+
+			OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+				if result.Landed() && spell.ProcMask&core.ProcMaskMelee != 0 {
+					triggeredDmgSpell.Cast(sim, result.Target)
+					return
+				}
+			},
+		})
+
+		mainSpell := character.GetOrRegisterSpell(core.SpellConfig{
+			ActionID: auraActionID,
+			Flags:    core.SpellFlagNoOnCastComplete,
+
+			Cast: core.CastConfig{
+				CD: core.Cooldown{
+					Timer:    character.NewTimer(),
+					Duration: time.Minute * 3,
+				},
+			},
+
+			ApplyEffects: func(sim *core.Simulation, _ *core.Unit, _ *core.Spell) {
+				mainAura.Activate(sim)
+			},
+		})
+
+		character.AddMajorCooldown(core.MajorCooldown{
+			Spell:    mainSpell,
+			Priority: core.CooldownPriorityDefault,
+			Type:     core.CooldownTypeDPS,
+		})
+	})
+
 	core.AddEffectsToTest = true
 }
