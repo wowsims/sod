8f6628d82a81a652af21941746577a582b4a5244
rosenrusinov <rainisunbelievable@gmail.com>
2024-02-24 02:00:13 +0100

    Update inputs.ts

--- ui/hunter/inputs.ts
+++ ui/hunter/inputs.ts
@@ -70,7 +70,7 @@ export const NewRaptorStrike = InputHelpers.makeSpecOptionsBooleanInput<Spec.Spe
 	fieldName: 'newRaptorStrike',
 	label: 'New Raptor Strike',
 	labelTooltip: 'New Raptor Strike with removed same weapon type 30% damage bonus.',
-	showWhen: (player) => player.getEquippedItem(ItemSlot.ItemSlotFeet)?.rune?.id == HunterRune.RuneBootsDualWieldSpecialization
+	showWhen: (player) => player.getEquippedItem(ItemSlot.ItemSlotFeet)?.rune?.id == HunterRune.RuneBootsDualWieldSpecialization,
 	changeEmitter: (player: Player<Spec.SpecHunter>) => TypedEvent.onAny([player.gearChangeEmitter, player.specOptionsChangeEmitter]),
 });
 
