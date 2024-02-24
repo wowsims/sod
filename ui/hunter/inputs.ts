f5a60c30b0f809c90db63b9337548deaa9ee0f69
rosenrusinov <rainisunbelievable@gmail.com>
2024-02-24 01:56:22 +0100

    fix rs input

--- ui/hunter/inputs.ts
+++ ui/hunter/inputs.ts
@@ -71,6 +71,7 @@ export const NewRaptorStrike = InputHelpers.makeSpecOptionsBooleanInput<Spec.Spe
 	label: 'New Raptor Strike',
 	labelTooltip: 'New Raptor Strike with removed same weapon type 30% damage bonus.',
 	showWhen: (player) => player.getEquippedItem(ItemSlot.ItemSlotFeet)?.rune?.id == HunterRune.RuneBootsDualWieldSpecialization
+	changeEmitter: (player: Player<Spec.SpecHunter>) => TypedEvent.onAny([player.gearChangeEmitter, player.specOptionsChangeEmitter]),
 });
 
 export const SniperTrainingUptime = InputHelpers.makeSpecOptionsNumberInput<Spec.SpecHunter>({
