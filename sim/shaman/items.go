8ff7e5e52899f24a489315405af731d45972807c
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-02-24 01:44:58 +0100

    Ancestral Bloodstorm Beacon on use

--- sim/shaman/items.go
+++ sim/shaman/items.go
@@ -1,6 +1,8 @@
 package shaman
 
 import (
+	"time"
+
 	"github.com/wowsims/sod/sim/core"
 	"github.com/wowsims/sod/sim/core/stats"
 )
@@ -15,5 +17,40 @@ func init() {
 	core.NewItemEffect(TotemCarvedDriftwoodIcon, func(agent core.Agent) {
 		character := agent.GetCharacter()
 		character.AddStat(stats.MP5, 2)
+	})
+
+	// Ancestral Bloodstorm Beacon
+	core.NewItemEffect(216615, func(agent core.Agent) {
+		character := agent.GetCharacter()
+
+		spell := character.RegisterSpell(core.SpellConfig{
+			ActionID:    core.ActionID{SpellID: 436413},
+			SpellSchool: core.SpellSchoolNature,
+			ProcMask:    core.ProcMaskSpellDamage,
+			Flags:       core.SpellFlagAPL,
+
+			Cast: core.CastConfig{
+				CD: core.Cooldown{
+					Timer:    character.NewTimer(),
+					Duration: time.Minute * 5,
+				},
+			},
+
+			DamageMultiplier: 1,
+			CritMultiplier:   character.DefaultSpellCritMultiplier(),
+
+			ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+				damage := 150 * character.PseudoStats.SchoolDamageDealtMultiplier[stats.SchoolIndexShadow]
+				for _, aoeTarget := range sim.Encounter.TargetUnits {
+					spell.CalcAndDealDamage(sim, aoeTarget, damage, spell.OutcomeMagicHitAndCrit)
+				}
+			},
+		})
+
+		character.AddMajorCooldown(core.MajorCooldown{
+			Spell:    spell,
+			Priority: core.CooldownPriorityLow,
+			Type:     core.CooldownTypeDPS,
+		})
 	})
 }
