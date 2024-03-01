0a443b994b0ffd19d6eaa1981dacb87aa752545c
rosenrusinov <rainisunbelievable@gmail.com>
2024-03-02 00:08:44 +0100

    update chimera,explosive and carve modifiers

--- sim/hunter/explosive_shot.go
+++ sim/hunter/explosive_shot.go
@@ -18,8 +18,8 @@ func (hunter *Hunter) registerExplosiveShotSpell(timer *core.Timer) {
 
 	level := float64(hunter.GetCharacter().Level)
 	baseCalc := (2.976264 + 0.641066*level + 0.022519*level*level)
-	baseLowDamage := baseCalc * 0.36
-	baseHighDamage := baseCalc * 0.54
+	baseLowDamage := baseCalc * 0.36 * 1.15 // Buff from 1/3/2024 - verify with new build and update numbers
+	baseHighDamage := baseCalc * 0.54 * 1.15
 
 	manaCostMultiplier := 1 - 0.02*float64(hunter.Talents.Efficiency)
 	if hunter.HasRune(proto.HunterRune_RuneChestMasterMarksman) {
