077c21dae8a9cefd9da771b583c086c1758306c6
Kayla Glick <kaylaglick12@gmail.com>
2024-03-15 23:14:35 -0400

    add tooltip + preset builds for hunter

--- ui/hunter/sim.ts
+++ ui/hunter/sim.ts
@@ -5,16 +5,7 @@ import * as Mechanics from '../core/constants/mechanics.js';
 import { Phase } from '../core/constants/other.js';
 import { IndividualSimUI, registerSpecConfig } from '../core/individual_sim_ui.js';
 import { Player } from '../core/player.js';
-import {
-	Class,
-	Faction,
-	ItemSlot,
-	PartyBuffs,
-	PseudoStat,
-	Race,
-	RangedWeaponType,
-	Spec,
-	Stat, } from '../core/proto/common.js';
+import { Class, Faction, ItemSlot, PartyBuffs, PseudoStat, Race, RangedWeaponType, Spec, Stat } from '../core/proto/common.js';
 import { HunterRune } from '../core/proto/hunter.js';
 import { Stats } from '../core/proto_utils/stats.js';
 import { getSpecIcon } from '../core/proto_utils/utils.js';
@@ -25,10 +16,8 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecHunter, {
 	cssClass: 'hunter-sim-ui',
 	cssScheme: 'hunter',
 	// List any known bugs / issues here and they'll be shown on the site.
-	knownIssues: [
-	],
-	warnings: [
-	],
+	knownIssues: [],
+	warnings: [],
 
 	// All stats for which EP should be calculated.
 	epStats: [
@@ -47,11 +36,7 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecHunter, {
 		Stat.StatNaturePower,
 		Stat.StatArcanePower,
 	],
-	epPseudoStats: [
-		PseudoStat.PseudoStatMainHandDps,
-		PseudoStat.PseudoStatOffHandDps,
-		PseudoStat.PseudoStatRangedDps,
-	],
+	epPseudoStats: [PseudoStat.PseudoStatMainHandDps, PseudoStat.PseudoStatOffHandDps, PseudoStat.PseudoStatRangedDps],
 	// Reference stat against which to calculate EP.
 	epReferenceStat: Stat.StatRangedAttackPower,
 	// Which stats to display in the Character Stats section, at the bottom of the left-hand sidebar.
@@ -95,26 +80,29 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecHunter, {
 		// Default equipped gear.
 		gear: Presets.DefaultGear.gear,
 		// Default EP weights for sorting gear in the gear picker.
-		epWeights: Stats.fromMap({
-			[Stat.StatStrength]: 0.30,
-			[Stat.StatAgility]: 0.64,
-			[Stat.StatStamina]: 0.0,
-			[Stat.StatIntellect]: 0.02,
-			[Stat.StatAttackPower]: 1,
-			[Stat.StatRangedAttackPower]: 1.0,
-			[Stat.StatMeleeHit]: 3.29,
-			[Stat.StatMeleeCrit]: 4.45,
-			[Stat.StatMeleeHaste]: 1.08,
-			[Stat.StatArmorPenetration]: 1.32,
-			[Stat.StatSpellPower]: 0.03,
-			[Stat.StatNaturePower]: 0.01,
-			[Stat.StatArcanePower]: 0.01,
-			[Stat.StatMP5]: 0.05,
-		}, {
-			[PseudoStat.PseudoStatMainHandDps]: 2.11,
-			[PseudoStat.PseudoStatOffHandDps]: 1.39,
-			[PseudoStat.PseudoStatRangedDps]: 6.32,
-		}),
+		epWeights: Stats.fromMap(
+			{
+				[Stat.StatStrength]: 0.3,
+				[Stat.StatAgility]: 0.64,
+				[Stat.StatStamina]: 0.0,
+				[Stat.StatIntellect]: 0.02,
+				[Stat.StatAttackPower]: 1,
+				[Stat.StatRangedAttackPower]: 1.0,
+				[Stat.StatMeleeHit]: 3.29,
+				[Stat.StatMeleeCrit]: 4.45,
+				[Stat.StatMeleeHaste]: 1.08,
+				[Stat.StatArmorPenetration]: 1.32,
+				[Stat.StatSpellPower]: 0.03,
+				[Stat.StatNaturePower]: 0.01,
+				[Stat.StatArcanePower]: 0.01,
+				[Stat.StatMP5]: 0.05,
+			},
+			{
+				[PseudoStat.PseudoStatMainHandDps]: 2.11,
+				[PseudoStat.PseudoStatOffHandDps]: 1.39,
+				[PseudoStat.PseudoStatRangedDps]: 6.32,
+			},
+		),
 		// Default consumes settings.
 		consumes: Presets.DefaultConsumes,
 		// Default talents.
@@ -130,24 +118,13 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecHunter, {
 	},
 
 	// IconInputs to include in the 'Player' section on the settings tab.
-	playerIconInputs: [
-		HunterInputs.PetTypeInput,
-		HunterInputs.WeaponAmmo,
-		HunterInputs.QuiverInput,
-	],
+	playerIconInputs: [HunterInputs.PetTypeInput, HunterInputs.WeaponAmmo, HunterInputs.QuiverInput],
 	// Inputs to include in the 'Rotation' section on the settings tab.
 	rotationInputs: HunterInputs.HunterRotationConfig,
-	petConsumeInputs: [
-		ConsumablesInputs.PetScrollOfAgility,
-		ConsumablesInputs.PetScrollOfStrength,
-	],
+	petConsumeInputs: [ConsumablesInputs.PetScrollOfAgility, ConsumablesInputs.PetScrollOfStrength],
 	// Buff and Debuff inputs to include/exclude, overriding the EP-based defaults.
-	includeBuffDebuffInputs: [
-		BuffDebuffInputs.SpellScorchDebuff,
-		BuffDebuffInputs.StaminaBuff,
-	],
-	excludeBuffDebuffInputs: [
-	],
+	includeBuffDebuffInputs: [BuffDebuffInputs.SpellScorchDebuff, BuffDebuffInputs.StaminaBuff],
+	excludeBuffDebuffInputs: [],
 	// Inputs to include in the 'Other' section on the settings tab.
 	otherInputs: {
 		inputs: [
@@ -167,29 +144,22 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecHunter, {
 
 	presets: {
 		// Preset talents that the user can quickly select.
-		talents: [
-			...Presets.TalentPresets[Phase.Phase2],
-			...Presets.TalentPresets[Phase.Phase1],
-		],
+		talents: [...Presets.TalentPresets[Phase.Phase2], ...Presets.TalentPresets[Phase.Phase1]],
 		// Preset rotations that the user can quickly select.
-		rotations: [
-			...Presets.APLPresets[Phase.Phase2],
-			...Presets.APLPresets[Phase.Phase1],
-		],
+		rotations: [...Presets.APLPresets[Phase.Phase2], ...Presets.APLPresets[Phase.Phase1]],
 		// Preset gear configurations that the user can quickly select.
-		gear: [
-			...Presets.GearPresets[Phase.Phase2],
-			...Presets.GearPresets[Phase.Phase1],
-		],
+		gear: [...Presets.GearPresets[Phase.Phase2], ...Presets.GearPresets[Phase.Phase1]],
+		builds: [Presets.PresetBuildRangedBM, Presets.PresetBuildRangedMM, Presets.PresetBuildMeleeSV],
 	},
 
 	autoRotation: player => {
-		const isMelee = player.getEquippedItem(ItemSlot.ItemSlotWaist)?.rune?.id == HunterRune.RuneBeltMeleeSpecialist &&
-			player.getEquippedItem(ItemSlot.ItemSlotFeet)?.rune?.id == HunterRune.RuneBootsDualWieldSpecialization
+		const isMelee =
+			player.getEquippedItem(ItemSlot.ItemSlotWaist)?.rune?.id == HunterRune.RuneBeltMeleeSpecialist &&
+			player.getEquippedItem(ItemSlot.ItemSlotFeet)?.rune?.id == HunterRune.RuneBootsDualWieldSpecialization;
 
 		if (isMelee) {
 			return Presets.DefaultAPLs[player.getLevel()][2].rotation.rotation!;
-		}else {
+		} else {
 			if (player.getTalentTree() == 1) {
 				return Presets.DefaultAPLs[player.getLevel()][1].rotation.rotation!;
 			} else {
