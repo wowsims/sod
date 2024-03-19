e26d9fd301dd3145b428c76442179ecbd7eebe17
rosenrusinov <rainisunbelievable@gmail.com>
2024-03-19 21:55:41 +0100

    Update Chaos Bolt to be partially resistable and add a 0 damage event to Serpent Sting

--- sim/warlock/chaos_bolt.go
+++ sim/warlock/chaos_bolt.go
@@ -22,7 +22,7 @@ func (warlock *Warlock) registerChaosBoltSpell() {
 		ActionID:    core.ActionID{SpellID: 403629},
 		SpellSchool: core.SpellSchoolFire,
 		ProcMask:    core.ProcMaskSpellDamage,
-		Flags:       core.SpellFlagAPL | core.SpellFlagResetAttackSwing | core.SpellFlagIgnoreResists,
+		Flags:       core.SpellFlagAPL | core.SpellFlagResetAttackSwing,
 
 		ManaCost: core.ManaCostOptions{
 			BaseCost:   0.07,
