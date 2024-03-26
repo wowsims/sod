74d6d0f32b961dfc0257c152a4b11617f7434fcb
Kayla Glick <kaylaglick12@gmail.com>
2024-03-26 18:03:29 -0400

    update presets for phase 3 launch

--- ui/balance_druid/presets.ts
+++ ui/balance_druid/presets.ts
@@ -1,4 +1,5 @@
 import { CURRENT_PHASE, Phase } from '../core/constants/other.js';
+import * as PresetUtils from '../core/preset_utils.js';
 import {
 	Consumes,
 	Debuffs,
@@ -13,15 +14,15 @@ import {
 	SpellPowerBuff,
 	TristateEffect,
 	UnitReference,
-	WeaponImbue
+	WeaponImbue,
 } from '../core/proto/common.js';
+import { BalanceDruid_Options as BalanceDruidOptions } from '../core/proto/druid.js';
 import { SavedTalents } from '../core/proto/ui.js';
-
-import {
-	BalanceDruid_Options as BalanceDruidOptions,
-} from '../core/proto/druid.js';
-
-import * as PresetUtils from '../core/preset_utils.js';
+import Phase1APL from './apls/phase_1.apl.json';
+import Phase2APL from './apls/phase_2.apl.json';
+import BlankGear from './gear_sets/blank.gear.json';
+import Phase1Gear from './gear_sets/phase_1.gear.json';
+import Phase2Gear from './gear_sets/phase_2.gear.json';
 
 // Preset options for this spec.
 // Eventually we will import these values for the raid sim too, so its good to
@@ -31,21 +32,13 @@ import * as PresetUtils from '../core/preset_utils.js';
 //                                 Gear Presets
 ///////////////////////////////////////////////////////////////////////////
 
-import BlankGear from './gear_sets/blank.gear.json';
-import Phase1Gear from './gear_sets/phase_1.gear.json';
-import Phase2Gear from './gear_sets/phase_2.gear.json';
-
 export const GearBlank = PresetUtils.makePresetGear('Blank', BlankGear);
 export const GearPhase1 = PresetUtils.makePresetGear('Phase 1', Phase1Gear);
 export const GearPhase2 = PresetUtils.makePresetGear('Phase 2', Phase2Gear);
 
 export const GearPresets = {
-  [Phase.Phase1]: [
-    GearPhase1,
-  ],
-  [Phase.Phase2]: [
-		GearPhase2,
-  ]
+	[Phase.Phase1]: [GearPhase1],
+	[Phase.Phase2]: [GearPhase2],
 };
 
 // TODO: Add Phase 2 preset and pull from map
@@ -55,25 +48,20 @@ export const DefaultGear = GearPresets[CURRENT_PHASE][0];
 //                                 APL Presets
 ///////////////////////////////////////////////////////////////////////////
 
-import Phase1APL from './apls/phase_1.apl.json';
-import Phase2APL from './apls/phase_2.apl.json';
-
 export const APLPhase1 = PresetUtils.makePresetAPLRotation('Phase 1', Phase1APL);
 export const APLPhase2 = PresetUtils.makePresetAPLRotation('Phase 2', Phase2APL);
 
 export const APLPresets = {
-  [Phase.Phase1]: [
-    APLPhase1,
-  ],
-  [Phase.Phase2]: [
-		APLPhase2,
-  ]
+	[Phase.Phase1]: [APLPhase1],
+	[Phase.Phase2]: [APLPhase2],
 };
 
 // TODO: Add Phase 2 preset and pull from map
 export const DefaultAPLs: Record<number, PresetUtils.PresetRotation> = {
-  25: APLPresets[Phase.Phase1][0],
-  40: APLPresets[Phase.Phase2][0],
+	25: APLPresets[Phase.Phase1][0],
+	40: APLPresets[Phase.Phase2][0],
+	// TODO: Phase 3
+	50: APLPresets[Phase.Phase2][0],
 };
 
 ///////////////////////////////////////////////////////////////////////////
@@ -98,12 +86,8 @@ export const TalentsPhase2 = {
 };
 
 export const TalentPresets = {
-  [Phase.Phase1]: [
-    TalentsPhase1,
-  ],
-  [Phase.Phase2]: [
-		TalentsPhase2
-  ]
+	[Phase.Phase1]: [TalentsPhase1],
+	[Phase.Phase2]: [TalentsPhase2],
 };
 
 // TODO: Add Phase 2 preset and pull from map
@@ -119,7 +103,7 @@ export const DefaultOptions = BalanceDruidOptions.create({
 
 export const DefaultConsumes = Consumes.create({
 	defaultPotion: Potions.GreaterManaPotion,
-  enchantedSigil: EnchantedSigil.InnovationSigil,
+	enchantedSigil: EnchantedSigil.InnovationSigil,
 	food: Food.FoodSagefishDelight,
 	mainHandImbue: WeaponImbue.LesserWizardOil,
 	spellPowerBuff: SpellPowerBuff.LesserArcaneElixir,
@@ -140,8 +124,7 @@ export const DefaultIndividualBuffs = IndividualBuffs.create({
 	saygesFortune: SaygesFortune.SaygesDamage,
 });
 
-export const DefaultPartyBuffs = PartyBuffs.create({
-});
+export const DefaultPartyBuffs = PartyBuffs.create({});
 
 export const DefaultDebuffs = Debuffs.create({
 	faerieFire: true,
