0c348f771a2232e2f3398bda4466339b8583a869
Kayla Glick <kaylaglick12@gmail.com>
2024-04-01 13:38:30 -0400

    make aura APL values explicitly return false if aura not known

--- sim/core/apl_values_aura.go
+++ sim/core/apl_values_aura.go
@@ -35,11 +35,6 @@ type APLValueAuraIsActive struct {
 
 func (rot *APLRotation) newValueAuraIsActive(config *proto.APLValueAuraIsActive) APLValue {
 	aura := rot.GetAPLAura(rot.GetSourceUnit(config.SourceUnit), config.AuraId)
-
-	if aura.Get() == nil {
-		return nil
-	}
-
 	return &APLValueAuraIsActive{
 		aura: aura,
 	}
@@ -48,7 +43,8 @@ func (value *APLValueAuraIsActive) Type() proto.APLValueType {
 	return proto.APLValueType_ValueTypeBool
 }
 func (value *APLValueAuraIsActive) GetBool(sim *Simulation) bool {
-	return value.aura.Get().IsActive()
+	aura := value.aura.Get()
+	return aura != nil && aura.IsActive()
 }
 func (value *APLValueAuraIsActive) String() string {
 	return fmt.Sprintf("Aura Active(%s)", value.aura.String())
@@ -62,9 +58,6 @@ type APLValueAuraIsActiveWithReactionTime struct {
 
 func (rot *APLRotation) newValueAuraIsActiveWithReactionTime(config *proto.APLValueAuraIsActiveWithReactionTime) APLValue {
 	aura := rot.GetAPLAura(rot.GetSourceUnit(config.SourceUnit), config.AuraId)
-	if aura.Get() == nil {
-		return nil
-	}
 	return &APLValueAuraIsActiveWithReactionTime{
 		aura:         aura,
 		reactionTime: rot.unit.ReactionTime,
@@ -75,7 +68,7 @@ func (value *APLValueAuraIsActiveWithReactionTime) Type() proto.APLValueType {
 }
 func (value *APLValueAuraIsActiveWithReactionTime) GetBool(sim *Simulation) bool {
 	aura := value.aura.Get()
-	return aura.IsActive() && aura.TimeActive(sim) >= value.reactionTime
+	return aura != nil && aura.IsActive() && aura.TimeActive(sim) >= value.reactionTime
 }
 func (value *APLValueAuraIsActiveWithReactionTime) String() string {
 	return fmt.Sprintf("Aura Active With Reaction Time(%s)", value.aura.String())
@@ -99,7 +92,8 @@ func (value *APLValueAuraRemainingTime) Type() proto.APLValueType {
 	return proto.APLValueType_ValueTypeDuration
 }
 func (value *APLValueAuraRemainingTime) GetDuration(sim *Simulation) time.Duration {
-	return value.aura.Get().RemainingDuration(sim)
+	aura := value.aura.Get()
+	return Ternary(aura == nil, 0, aura.RemainingDuration(sim))
 }
 func (value *APLValueAuraRemainingTime) String() string {
 	return fmt.Sprintf("Aura Remaining Time(%s)", value.aura.String())
@@ -111,23 +105,22 @@ type APLValueAuraNumStacks struct {
 }
 
 func (rot *APLRotation) newValueAuraNumStacks(config *proto.APLValueAuraNumStacks) APLValue {
-	aura := rot.GetAPLAura(rot.GetSourceUnit(config.SourceUnit), config.AuraId)
-	if aura.Get() == nil {
-		return nil
-	}
-	if aura.Get().MaxStacks == 0 {
+	auraRef := rot.GetAPLAura(rot.GetSourceUnit(config.SourceUnit), config.AuraId)
+	aura := auraRef.Get()
+	if aura != nil && aura.MaxStacks == 0 {
 		rot.ValidationWarning("%s is not a stackable aura", ProtoToActionID(config.AuraId))
 		return nil
 	}
 	return &APLValueAuraNumStacks{
-		aura: aura,
+		aura: auraRef,
 	}
 }
 func (value *APLValueAuraNumStacks) Type() proto.APLValueType {
 	return proto.APLValueType_ValueTypeInt
 }
 func (value *APLValueAuraNumStacks) GetInt(sim *Simulation) int32 {
-	return value.aura.Get().GetStacks()
+	aura := value.aura.Get()
+	return Ternary(aura == nil, 0, aura.GetStacks())
 }
 func (value *APLValueAuraNumStacks) String() string {
 	return fmt.Sprintf("Aura Num Stacks(%s)", value.aura.String())
@@ -140,9 +133,6 @@ type APLValueAuraInternalCooldown struct {
 
 func (rot *APLRotation) newValueAuraInternalCooldown(config *proto.APLValueAuraInternalCooldown) APLValue {
 	aura := rot.GetAPLICDAura(rot.GetSourceUnit(config.SourceUnit), config.AuraId)
-	if aura.Get() == nil {
-		return nil
-	}
 	return &APLValueAuraInternalCooldown{
 		aura: aura,
 	}
@@ -151,7 +141,8 @@ func (value *APLValueAuraInternalCooldown) Type() proto.APLValueType {
 	return proto.APLValueType_ValueTypeDuration
 }
 func (value *APLValueAuraInternalCooldown) GetDuration(sim *Simulation) time.Duration {
-	return value.aura.Get().Icd.TimeToReady(sim)
+	aura := value.aura.Get()
+	return Ternary(aura == nil, time.Duration(0), aura.Icd.TimeToReady(sim))
 }
 func (value *APLValueAuraInternalCooldown) String() string {
 	return fmt.Sprintf("Aura Remaining ICD(%s)", value.aura.String())
@@ -165,9 +156,6 @@ type APLValueAuraICDIsReadyWithReactionTime struct {
 
 func (rot *APLRotation) newValueAuraICDIsReadyWithReactionTime(config *proto.APLValueAuraICDIsReadyWithReactionTime) APLValue {
 	aura := rot.GetAPLICDAura(rot.GetSourceUnit(config.SourceUnit), config.AuraId)
-	if aura.Get() == nil {
-		return nil
-	}
 	return &APLValueAuraICDIsReadyWithReactionTime{
 		aura:         aura,
 		reactionTime: rot.unit.ReactionTime,
@@ -178,7 +166,7 @@ func (value *APLValueAuraICDIsReadyWithReactionTime) Type() proto.APLValueType {
 }
 func (value *APLValueAuraICDIsReadyWithReactionTime) GetBool(sim *Simulation) bool {
 	aura := value.aura.Get()
-	return aura.Icd.IsReady(sim) || (aura.IsActive() && aura.TimeActive(sim) < value.reactionTime)
+	return aura != nil && aura.Icd.IsReady(sim) || (aura.IsActive() && aura.TimeActive(sim) < value.reactionTime)
 }
 func (value *APLValueAuraICDIsReadyWithReactionTime) String() string {
 	return fmt.Sprintf("Aura ICD Is Ready with Reaction Time(%s)", value.aura.String())
@@ -192,9 +180,6 @@ type APLValueAuraShouldRefresh struct {
 
 func (rot *APLRotation) newValueAuraShouldRefresh(config *proto.APLValueAuraShouldRefresh) APLValue {
 	aura := rot.GetAPLAura(rot.GetTargetUnit(config.SourceUnit), config.AuraId)
-	if aura.Get() == nil {
-		return nil
-	}
 
 	maxOverlap := rot.coerceTo(rot.newAPLValue(config.MaxOverlap), proto.APLValueType_ValueTypeDuration)
 	if maxOverlap == nil {
@@ -213,7 +198,8 @@ func (value *APLValueAuraShouldRefresh) Type() proto.APLValueType {
 	return proto.APLValueType_ValueTypeBool
 }
 func (value *APLValueAuraShouldRefresh) GetBool(sim *Simulation) bool {
-	return value.aura.Get().ShouldRefreshExclusiveEffects(sim, value.maxOverlap.GetDuration(sim))
+	aura := value.aura.Get()
+	return aura != nil && aura.ShouldRefreshExclusiveEffects(sim, value.maxOverlap.GetDuration(sim))
 }
 func (value *APLValueAuraShouldRefresh) String() string {
 	return fmt.Sprintf("Should Refresh Aura(%s)", value.aura.String())
