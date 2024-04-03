b94e2e8118067c5f9ff16db960288a2520e73449
Kayla Glick <kaylaglick12@gmail.com>
2024-04-03 11:36:58 -0400

    cache rune equipped state since that builds before the pull

--- sim/core/apl_values_rune.go
+++ sim/core/apl_values_rune.go
@@ -7,15 +7,21 @@ import (
 	"github.com/wowsims/sod/sim/core/proto"
 )
 
+type APLRune struct {
+	id       int32
+	equipped bool
+}
+
 type APLValueRuneIsEquipped struct {
 	DefaultAPLValueImpl
 	character *Character
-	rune      Rune
+	rune      APLRune
 }
 
 func (rot *APLRotation) newValueRuneIsEquipped(config *proto.APLValueRuneIsEquipped) APLValue {
 	character := rot.unit.Env.Raid.GetPlayerFromUnit(rot.unit).GetCharacter()
-	rune := rot.GetAPLRune(config.GetRuneId())
+	spellId := config.GetRuneId().GetSpellId()
+	rune := APLRune{id: spellId, equipped: slices.Contains(character.Equipment.GetRuneIds(), spellId)}
 
 	return &APLValueRuneIsEquipped{
 		character: character,
@@ -26,8 +32,8 @@ func (value *APLValueRuneIsEquipped) Type() proto.APLValueType {
 	return proto.APLValueType_ValueTypeBool
 }
 func (value *APLValueRuneIsEquipped) GetBool(sim *Simulation) bool {
-	return slices.Contains(value.character.Equipment.GetRuneIds(), value.rune.ID)
+	return value.rune.equipped
 }
 func (value *APLValueRuneIsEquipped) String() string {
-	return fmt.Sprintf("Rune Equipped(%d)", value.rune.ID)
+	return fmt.Sprintf("Rune Equipped(%d)", value.rune.id)
 }
