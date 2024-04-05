fa338f784c23c347a3f2b225544643c669d28698
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-04-05 06:22:38 +0200

    Scorpid Poison coef

--- sim/hunter/pet_abilities.go
+++ sim/hunter/pet_abilities.go
@@ -299,12 +299,13 @@ func (hp *HunterPet) newScorpidPoison() *core.Spell {
 			Aura: core.Aura{
 				Label: "ScorpidPoison",
 			},
-			NumberOfTicks: 5,
-			TickLength:    time.Second * 2,
+			NumberOfTicks:    5,
+			TickLength:       time.Second * 2,
+			BonusCoefficient: 1,
 			OnSnapshot: func(sim *core.Simulation, target *core.Unit, dot *core.Dot, isRollover bool) {
-				dot.SnapshotBaseDamage = sim.Roll(100/5, 130/5) + (0.07/5)*dot.Spell.MeleeAttackPower()
-				dot.SnapshotBaseDamage *= hp.killCommandMult()
-				dot.SnapshotAttackerMultiplier = dot.Spell.AttackerDamageMultiplier(dot.Spell.Unit.AttackTables[target.UnitIndex][dot.Spell.CastType])
+				damage := sim.Roll(100/5, 130/5) + (0.07/5)*dot.Spell.MeleeAttackPower()
+				dot.Snapshot(target, damage, isRollover)
+				dot.SnapshotAttackerMultiplier *= hp.killCommandMult()
 			},
 			OnTick: func(sim *core.Simulation, target *core.Unit, dot *core.Dot) {
 				dot.CalcAndDealPeriodicSnapshotDamage(sim, target, dot.OutcomeTick)
