5f0cacb9d4c50d6743c7e624e898b20ce8303531
Kayla Glick <kaylaglick12@gmail.com>
2024-04-08 17:17:54 -0400

    update banner

--- ui/core/individual_sim_ui.ts
+++ ui/core/individual_sim_ui.ts
@@ -187,7 +187,7 @@ export abstract class IndividualSimUI<SpecType extends Spec> extends SimUI {
 			spec: player.spec,
 			knownIssues: config.knownIssues,
 			simStatus: simLaunchStatuses[player.spec],
-			noticeText: `We're working to implement the many new gear changes that were just released. Many procs and effects may no longer be accurate in the sim.`,
+			noticeText: `All item sets are now implemented and we're working to finish item effects and update consumables.`,
 		});
 		this.rootElem.classList.add('individual-sim-ui');
 		this.player = player;
