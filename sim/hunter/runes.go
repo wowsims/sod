24251ffeb9a1a5b47950484e58ccba2c4969ecc1
jeff clements <jeffmclements@icloud.com>
2024-04-30 09:41:13 -0700

    updated hunter runes for 4/30

--- sim/hunter/runes.go
+++ sim/hunter/runes.go
@@ -29,7 +29,7 @@ func (hunter *Hunter) ApplyRunes() {
 	}
 
 	if hunter.HasRune(proto.HunterRune_RuneHandsBeastmastery) && hunter.pet != nil {
-		hunter.pet.PseudoStats.DamageDealtMultiplier *= 1.2
+		hunter.pet.PseudoStats.DamageDealtMultiplier *= 1.1
 	}
 
 	if hunter.HasRune(proto.HunterRune_RuneBootsDualWieldSpecialization) {
