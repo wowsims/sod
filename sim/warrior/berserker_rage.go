2e3222f5836076e8528f552f808a9d6530dca886
Emrus <emrus@outlook.com>
2024-05-10 03:32:47 +0200

    Adds rage gain from Berserker Rage

--- sim/warrior/berserker_rage.go
+++ sim/warrior/berserker_rage.go
@@ -12,11 +12,21 @@ func (warrior *Warrior) registerBerserkerRageSpell() {
 	}
 
 	actionID := core.ActionID{SpellID: 18499}
+	rageMetrics := warrior.NewRageMetrics(actionID)
+	rageMultiplier := 1.0
 
 	warrior.BerserkerRageAura = warrior.RegisterAura(core.Aura{
 		Label:    "Berserker Rage",
 		ActionID: actionID,
 		Duration: time.Second * 10,
+
+		// Copy from rage.go
+		OnSpellHitTaken: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+			rageConversionTaken := 0.0091107836*float64(spell.Unit.Level^2) + 3.225598133*float64(spell.Unit.Level) + 4.2652911
+			generatedRage := result.Damage * 2.5 / rageConversionTaken
+			generatedRage *= rageMultiplier
+			warrior.AddRage(sim, generatedRage, rageMetrics)
+		},
 	})
 
 	warrior.BerserkerRage = warrior.RegisterSpell(core.SpellConfig{
