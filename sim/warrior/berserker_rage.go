1576c69b5b5c255e25cdb5402d6f3e225f3e9b32
Emrus <emrus@outlook.com>
2024-05-15 16:26:22 +0200

    Updated Berserker Rage rage generation

--- sim/warrior/berserker_rage.go
+++ sim/warrior/berserker_rage.go
@@ -14,7 +14,6 @@ func (warrior *Warrior) registerBerserkerRageSpell() {
 	actionID := core.ActionID{SpellID: 18499}
 	rageMetrics := warrior.NewRageMetrics(actionID)
 	instantRage := 5 * float64(warrior.Talents.ImprovedBerserkerRage)
-	rageMultiplier := 1.0
 
 	warrior.BerserkerRageAura = warrior.RegisterAura(core.Aura{
 		Label:    "Berserker Rage",
@@ -22,10 +21,21 @@ func (warrior *Warrior) registerBerserkerRageSpell() {
 		Duration: time.Second * 10,
 
 		OnSpellHitTaken: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
-			rageConversionDamageTaken := core.GetRageConversion(spell.Unit.Level)
-			generatedRage := result.Damage * 2.5 / rageConversionDamageTaken
-			generatedRage *= rageMultiplier
-			warrior.AddRage(sim, generatedRage, rageMetrics)
+			if !result.Landed() || result.Damage <= 0 {
+				return
+			}
+
+			if spell.ProcMask.Matches(core.ProcMaskMeleeOrRanged) {
+				// For melee attacks we find that it gives around 2.0 extra rage regardless of attacker level
+				// This may give less rage for fast attacks, but we default to 2.0 for now
+				warrior.AddRage(sim, 2.0, rageMetrics)
+			} else if spell.ProcMask.Matches(core.ProcMaskSpellDamage) {
+				// Spell attacks generally give 1 - 2 times unmodified damage as additional rage.
+				rageConversionDamageTaken := core.GetRageConversion(spell.Unit.Level)
+				generatedRage := result.RawDamage() * 2.5 / rageConversionDamageTaken
+				generatedRage *= 1.0 // Using 1.0 because we don't know why it gives more sometimes
+				warrior.AddRage(sim, generatedRage, rageMetrics)
+			}
 		},
 	})
 
