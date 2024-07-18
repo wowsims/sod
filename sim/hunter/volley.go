b00993166838422d783053c202a6fd411ede53c1
ncberman <ncberman@asu.edu>
2024-07-18 00:15:48 +0000

    implemented volley spell + rune

---
+++ sim/hunter/volley.go
@@ -0,0 +1,102 @@
+package hunter
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/wowsims/sod/sim/core"
+	"github.com/wowsims/sod/sim/core/proto"
+	"github.com/wowsims/sod/sim/core/stats"
+)
+
+func (hunter *Hunter) registerVolleySpell() {
+	ranks := 3
+
+	for i := ranks; i >= 0; i-- {
+		config := hunter.getVolleyConfig(i)
+
+		if config.RequiredLevel <= int(hunter.Level) {
+			hunter.Volley = hunter.GetOrRegisterSpell(config)
+			break
+		}
+	}
+}
+
+func (hunter *Hunter) getVolleyConfig(rank int) core.SpellConfig {
+	spellId := [4]int32{0, 1510, 14294, 14295}[rank]
+	baseDamage := [4]float64{0, 50, 65, 80}[rank]
+	manaCost := [4]float64{0, 350, 420, 490}[rank]
+	level := [4]int{0, 40, 50, 58}[rank]
+
+	hasImprovedVolley := hunter.HasRune(proto.HunterRune_RuneCloakImprovedVolley)
+
+	manaCostModifer := 1.0
+	manaCostModifer *= 1 - (0.02 * float64(hunter.Talents.Efficiency))
+
+	if hasImprovedVolley {
+		manaCostModifer *= 0.5
+	}
+
+
+	return core.SpellConfig{
+		ActionID:    core.ActionID{SpellID: spellId},
+		SpellSchool: core.SpellSchoolArcane,
+		ProcMask:    core.ProcMaskSpellDamage,
+		Flags:       core.SpellFlagChanneled | core.SpellFlagAPL,
+
+		RequiredLevel: level,
+		Rank:          rank,
+
+		ManaCost: core.ManaCostOptions{
+			FlatCost: manaCost,
+			Multiplier: manaCostModifer,
+		},
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD: core.GCDDefault,
+			},
+			CD: core.Cooldown{
+				Timer:    hunter.NewTimer(),
+				Duration: time.Second * 60,
+			},
+		},
+		ExtraCastCondition: func(sim *core.Simulation, target *core.Unit) bool {
+			return !hunter.Unit.Moving
+		},
+
+		Dot: core.DotConfig{
+			IsAOE: true,
+			Aura: core.Aura{
+				Label: fmt.Sprintf("Volley (Rank %d)", rank),
+			},
+			NumberOfTicks:    6,
+			TickLength:       time.Second * 1,
+			BonusCoefficient: .056,
+			OnSnapshot: func(sim *core.Simulation, target *core.Unit, dot *core.Dot, isRollover bool) {
+				damage := baseDamage
+				if hasImprovedVolley {
+					damage += hunter.GetStat(stats.RangedAttackPower) * 0.03
+				}
+				dot.Snapshot(target, damage, isRollover)
+			},
+			OnTick: func(sim *core.Simulation, target *core.Unit, dot *core.Dot) {
+				for _, aoeTarget := range sim.Encounter.TargetUnits {
+					dot.CalcAndDealPeriodicSnapshotDamage(sim, aoeTarget, dot.OutcomeTick)
+				}
+			},
+		},
+
+		CritDamageBonus: (1 + hunter.mortalShots()) * (1 + (0.05 * float64(hunter.Talents.Barrage))),
+		DamageMultiplier: 1, 
+		ThreatMultiplier: 1,
+
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+			if hasImprovedVolley {
+				spell.CD.Reset()
+			}
+			hunter.Unit.AutoAttacks.DelayRangedUntil(sim, sim.CurrentTime + (time.Second * 6))
+			spell.AOEDot().Apply(sim)
+		},
+		
+	}
+}
