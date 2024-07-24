d6b9dcee0b30bc9c763132dfd9ca7cb07ec77956
Adam Chodaba <adamrch44@gmail.com>
2024-07-23 23:57:30 -0400

    fix consumable and buff defaults

--- ui/retribution_paladin/presets.ts
+++ ui/retribution_paladin/presets.ts
@@ -142,7 +142,7 @@ export const DefaultTalents = TalentPresets[Phase.Phase4][0];
 ///////////////////////////////////////////////////////////////////////////
 
 export const DefaultOptions = RetributionPaladinOptions.create({
-	aura: PaladinAura.RetributionAura,
+	aura: PaladinAura.SanctityAura,
 	primarySeal: PaladinSeal.Martyrdom,
 });
 
@@ -152,10 +152,10 @@ export const DefaultConsumes = Consumes.create({
 	defaultPotion: Potions.MajorManaPotion,
 	dragonBreathChili: true,
 	enchantedSigil: EnchantedSigil.LivingDreamsSigil,
-	fillerExplosive: Explosive.ExplosiveEzThroRadiationBomb,
 	food: Food.FoodBlessSunfruit,
 	flask: Flask.FlaskOfSupremePower,
 	firePowerBuff: FirePowerBuff.ElixirOfGreaterFirepower,
+	fillerExplosive: Explosive.ExplosiveUnknown,
 	mainHandImbue: WeaponImbue.WildStrikes,
 	spellPowerBuff: SpellPowerBuff.GreaterArcaneElixir,
 	strengthBuff: StrengthBuff.JujuPower,
@@ -194,12 +194,16 @@ export const DefaultRaidBuffs = RaidBuffs.create({
 export const DefaultDebuffs = Debuffs.create({
 	curseOfRecklessness: true,
 	homunculi: 70, // 70% average uptime default
+	faerieFire: true,
+	giftOfArthas: true,
 	sunderArmor: true,
 	judgementOfWisdom: true,
+	judgementOfTheCrusader: TristateEffect.TristateEffectImproved,
 	improvedFaerieFire: true,
 	improvedScorch: true,
 	markOfChaos: true,
 	occultPoison: true,
+	mangle: true,
 });
 
 export const OtherDefaults = {
