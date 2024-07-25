c751793fe6e90e558ace458fe946d79de39dddc9
Kayla Glick <kaylaglick12@gmail.com>
2024-07-25 19:27:01 -0400

    add fire res to balance

--- ui/balance_druid/presets.ts
+++ ui/balance_druid/presets.ts
@@ -122,6 +122,8 @@ export const DefaultRaidBuffs = RaidBuffs.create({
 	aspectOfTheLion: true,
 	demonicPact: 80,
 	divineSpirit: true,
+	fireResistanceAura: true,
+	fireResistanceTotem: true,
 	giftOfTheWild: TristateEffect.TristateEffectImproved,
 	manaSpringTotem: TristateEffect.TristateEffectRegular,
 	moonkinAura: true,
