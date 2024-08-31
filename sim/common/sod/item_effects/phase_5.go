90abab3ab66a041a8812bf9a2c348d45eca3b2b2
Kayla Glick <kaylaglick12@gmail.com>
2024-08-31 17:20:01 -0400

    add stormwrath dagger effect

--- sim/common/sod/item_effects/phase_5.go
+++ sim/common/sod/item_effects/phase_5.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	Stormwrath     = 231387
 	LightningsCell = 231784
 )
 
@@ -16,6 +17,49 @@ func init() {
 	///////////////////////////////////////////////////////////////////////////
 	//                                 Weapons
 	///////////////////////////////////////////////////////////////////////////
+
+	// https://www.wowhead.com/classic/item=231387/stormwrath-sanctified-shortblade-of-the-galefinder
+	// Equip: Damaging non-periodic spells have a chance to blast up to 3 targets for 181 to 229.
+	// (Proc chance: 10%, 100ms cooldown)
+	core.NewItemEffect(Stormwrath, func(agent core.Agent) {
+		character := agent.GetCharacter()
+
+		maxHits := int(min(3, character.Env.GetNumTargets()))
+		procSpell := character.RegisterSpell(core.SpellConfig{
+			ActionID:         core.ActionID{SpellID: 468670},
+			SpellSchool:      core.SpellSchoolNature,
+			DefenseType:      core.DefenseTypeMagic,
+			ProcMask:         core.ProcMaskEmpty,
+			BonusCoefficient: 0.15,
+			DamageMultiplier: 1,
+			ThreatMultiplier: 1,
+			ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+				for numHits := 0; numHits < maxHits; numHits++ {
+					spell.CalcAndDealDamage(sim, target, sim.Roll(180, 230), spell.OutcomeMagicHitAndCrit)
+					target = character.Env.NextTargetUnit(target)
+				}
+			},
+		})
+
+		icd := core.Cooldown{
+			Timer:    character.NewTimer(),
+			Duration: time.Millisecond * 100,
+		}
+		core.MakeProcTriggerAura(&character.Unit, core.ProcTrigger{
+			Name:       "Chain Lightning (Stormwrath)",
+			Callback:   core.CallbackOnSpellHitDealt,
+			Outcome:    core.OutcomeLanded,
+			ProcMask:   core.ProcMaskSpellDamage,
+			ProcChance: .10,
+			Handler: func(sim *core.Simulation, _ *core.Spell, result *core.SpellResult) {
+				if !icd.IsReady(sim) {
+					return
+				}
+				procSpell.Cast(sim, result.Target)
+				icd.Use(sim)
+			},
+		})
+	})
 
 	///////////////////////////////////////////////////////////////////////////
 	//                                 Trinkets
