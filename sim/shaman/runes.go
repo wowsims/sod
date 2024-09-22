23c1937db05920be2fb7030e0d147a04364967cb
Kayla Glick <kaylaglick12@gmail.com>
2024-09-22 19:44:19 -0400

    update maelstrom weapon trigger

--- sim/shaman/runes.go
+++ sim/shaman/runes.go
@@ -337,13 +337,9 @@ func (shaman *Shaman) applyMaelstromWeapon() {
 
 	// This aura is hidden, just applies stacks of the proc aura.
 	core.MakePermanent(shaman.RegisterAura(core.Aura{
-		Label: "MaelstromWeapon",
+		Label: "Maelstrom Weapon Trigger",
 		OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
-			if !result.Landed() {
-				return
-			}
-
-			if shaman.maelstromWeaponPPMM.Proc(sim, spell.ProcMask, "Maelstrom Weapon") {
+			if result.Landed() && spell.ProcMask.Matches(core.ProcMaskMelee) && shaman.maelstromWeaponPPMM.Proc(sim, spell.ProcMask, "Maelstrom Weapon") {
 				shaman.MaelstromWeaponAura.Activate(sim)
 				shaman.MaelstromWeaponAura.AddStack(sim)
 			}
