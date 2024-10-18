d80d5d056d974ce1a00f299408e0105a56dec418
Nathan Berman <ncberman@asu.edu>
2024-10-18 23:47:29 +0000

    Improved SS was working as expected before, small reversion

--- sim/hunter/serpent_sting.go
+++ sim/hunter/serpent_sting.go
@@ -11,7 +11,6 @@ import (
 func (hunter *Hunter) getSerpentStingConfig(rank int) core.SpellConfig {
 	spellId := [10]int32{0, 1978, 13549, 13550, 13551, 13552, 13553, 13554, 13555, 25295}[rank]
 	baseDamage := [10]float64{0, 20, 40, 80, 140, 210, 290, 385, 490, 555}[rank] / 5
-	baseDamage *= 1 + 0.02*float64(hunter.Talents.ImprovedSerpentSting)
 	spellCoeff := [10]float64{0, .4, .625, .925, 1, 1, 1, 1, 1, 1}[rank] / 5
 	manaCost := [10]float64{0, 15, 30, 50, 80, 115, 150, 190, 230, 250}[rank]
 	level := [10]int{0, 4, 10, 18, 26, 34, 42, 50, 58, 60}[rank]
@@ -20,7 +19,7 @@ func (hunter *Hunter) getSerpentStingConfig(rank int) core.SpellConfig {
 		ActionID:      core.ActionID{SpellID: spellId},
 		SpellSchool:   core.SpellSchoolNature,
 		DefenseType:   core.DefenseTypeRanged,
-		ProcMask:      core.ProcMaskRangedSpecial,
+		ProcMask:      core.ProcMaskEmpty,
 		Flags:         core.SpellFlagAPL | core.SpellFlagPureDot | core.SpellFlagPoison | SpellFlagSting,
 		CastType:      proto.CastType_CastTypeRanged,
 		Rank:          rank,
@@ -40,7 +39,7 @@ func (hunter *Hunter) getSerpentStingConfig(rank int) core.SpellConfig {
 			return hunter.DistanceFromTarget >= core.MinRangedAttackDistance
 		},
 
-		DamageMultiplier: 1,
+		DamageMultiplier: 1 + 0.02*float64(hunter.Talents.ImprovedSerpentSting),
 		ThreatMultiplier: 1,
 
 		Dot: core.DotConfig{
@@ -78,7 +77,6 @@ func (hunter *Hunter) getSerpentStingConfig(rank int) core.SpellConfig {
 
 func (hunter *Hunter) chimeraShotSerpentStingSpell(rank int) *core.Spell {
 	baseDamage := [10]float64{0, 20, 40, 80, 140, 210, 290, 385, 490, 555}[rank]
-	baseDamage *= 1 + 0.02*float64(hunter.Talents.ImprovedSerpentSting)
 	spellCoeff := [10]float64{0, .4, .625, .925, 1, 1, 1, 1, 1, 1}[rank]
 	return hunter.RegisterSpell(core.SpellConfig{
 		ActionID:    core.ActionID{SpellID: 409493},
@@ -93,6 +91,7 @@ func (hunter *Hunter) chimeraShotSerpentStingSpell(rank int) *core.Spell {
 		CritDamageBonus: hunter.mortalShots(),
 
 		DamageMultiplier:         0.48,
+		DamageMultiplierAdditive: 1 + 0.02*float64(hunter.Talents.ImprovedSerpentSting),
 		ThreatMultiplier:         1,
 		BonusCoefficient:         spellCoeff,
 
