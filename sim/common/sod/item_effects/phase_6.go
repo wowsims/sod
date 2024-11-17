8419acb762a3a59111f946ccd774459c663a1ad8
Kayla Glick <kaylaglick12@gmail.com>
2024-11-17 18:49:03 -0500

    add obsidian weapons, multiple new ring and armor proc effects

--- sim/common/sod/item_effects/phase_6.go
+++ sim/common/sod/item_effects/phase_6.go
@@ -3,11 +3,26 @@ package item_effects
 import (
 	"time"
 
+	"github.com/wowsims/sod/sim/common/sod"
+	"github.com/wowsims/sod/sim/common/vanilla"
 	"github.com/wowsims/sod/sim/core"
 	"github.com/wowsims/sod/sim/core/stats"
 )
 
 const (
+	LeggingsOfImmersion   = 233505
+	RingOfSwarmingThought = 233507
+	RobesOfTheBattleguard = 233575
+
+	// Obsidian Weapons
+	ObsidianChampion    = 233490
+	ObsidianReaver      = 233491
+	ObsidianDestroyer   = 233796
+	ObsidianStormhammer = 233797
+	ObsidianSageblade   = 233798
+	ObsidianDefender    = 233801
+	ObsidianHeartseeker = 234428
+
 	// Brood of Nozdormu Reputations Rings
 	SignetRingBronzeDominatorR5   = 234034
 	SignetRingBronzeDominatorR4   = 234030
@@ -50,8 +65,107 @@ func init() {
 	core.AddEffectsToTest = false
 
 	///////////////////////////////////////////////////////////////////////////
+	//                                 Weapons
+	///////////////////////////////////////////////////////////////////////////
+
+	// https://www.wowhead.com/classic/item=233490/obsidian-champion
+	// Chance on hit: Heal self for 270 to 450 and Increases Strength by 120 for 30 sec.
+	// Chance on hit: Increases damage done by 20 and attack speed by 5% for 15 sec.
+	// Equip: Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+	// 		  Deals extra damage to Silithid creatures in Silithus on almost every hit.
+	// 		  (2.1s cooldown)
+	core.NewItemEffect(ObsidianChampion, func(agent core.Agent) {
+		character := agent.GetCharacter()
+		vanilla.StrengthOfTheChampionAura(character)
+		vanilla.EnrageAura446327(character)
+		ObsidianEdgedAura(ObsidianChampion, agent)
+	})
+
+	// https://www.wowhead.com/classic/item=233801/obsidian-defender
+	// Equip: Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+	// 		  Deals extra damage to Silithid creatures in Silithus on almost every hit.
+	// 		  (2.1s cooldown)
+	core.NewItemEffect(ObsidianDefender, func(agent core.Agent) {
+		ObsidianEdgedAura(ObsidianDefender, agent)
+	})
+
+	// https://www.wowhead.com/classic/item=233796/obsidian-destroyer
+	// Chance on hit: Stuns target for 3 sec.
+	// Equip: Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+	// 		  Deals extra damage to Silithid creatures in Silithus on almost every hit.
+	// 		  (2.1s cooldown)
+	core.NewItemEffect(ObsidianDestroyer, func(agent core.Agent) {
+		ObsidianEdgedAura(ObsidianDestroyer, agent)
+	})
+
+	// https://www.wowhead.com/classic/item=234428/obsidian-heartseeker
+	// Equip: Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+	// 		  Deals extra damage to Silithid creatures in Silithus on almost every hit.
+	// 		  (2.1s cooldown)
+	core.NewItemEffect(ObsidianHeartseeker, func(agent core.Agent) {
+		ObsidianEdgedAura(ObsidianHeartseeker, agent)
+	})
+
+	// https://www.wowhead.com/classic/item=233491/obsidian-reaver
+	// Equip: Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+	// 		  Deals extra damage to Silithid creatures in Silithus on almost every hit.
+	// 		  (2.1s cooldown)
+	core.NewItemEffect(ObsidianReaver, func(agent core.Agent) {
+		ObsidianEdgedAura(ObsidianReaver, agent)
+	})
+
+	// https://www.wowhead.com/classic/item=233798/obsidian-sageblade
+	// Equip: Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+	// 		  Deals extra damage to Silithid creatures in Silithus on almost every hit.
+	// 		  (2.1s cooldown)
+	core.NewItemEffect(ObsidianSageblade, func(agent core.Agent) {
+		ObsidianEdgedAura(ObsidianSageblade, agent)
+	})
+
+	// https://www.wowhead.com/classic/item=233797/obsidian-stormhammer
+	// Chance on hit: Blasts up to 3 targets for 105 to 145 Nature damage.
+	// Equip: Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+	// 		  Deals extra damage to Silithid creatures in Silithus on almost every hit.
+	// 		  (2.1s cooldown)
+	core.NewItemEffect(ObsidianStormhammer, func(agent core.Agent) {
+		sod.StormhammerChainLightningProcAura(agent)
+		ObsidianEdgedAura(ObsidianStormhammer, agent)
+	})
+
+	///////////////////////////////////////////////////////////////////////////
 	//                                 Rings
 	///////////////////////////////////////////////////////////////////////////
+
+	// https://www.wowhead.com/classic/item=233507/ring-of-swarming-thought
+	// Your damaging non-periodic spells increase your spell damage by 15 for 15 sec.
+	// If the target is player controlled, gain 100 spell penetration for 15 sec instead.
+	// (15s cooldown)
+	core.NewItemEffect(RingOfSwarmingThought, func(agent core.Agent) {
+		character := agent.GetCharacter()
+		buffAura := character.RegisterAura(core.Aura{
+			ActionID: core.ActionID{SpellID: 474148},
+			Label:    "Swarming Thoughts",
+			Duration: time.Second * 15,
+			OnGain: func(aura *core.Aura, sim *core.Simulation) {
+				character.AddStatDynamic(sim, stats.SpellDamage, 15)
+			},
+			OnExpire: func(aura *core.Aura, sim *core.Simulation) {
+				character.AddStatDynamic(sim, stats.SpellDamage, -15)
+			},
+		})
+
+		core.MakeProcTriggerAura(&agent.GetCharacter().Unit, core.ProcTrigger{
+			Name:       "Swarming Thoughts Trigger",
+			Callback:   core.CallbackOnSpellHitDealt,
+			Outcome:    core.OutcomeLanded,
+			ProcMask:   core.ProcMaskSpellDamage,
+			ProcChance: 1.00,
+			ICD:        time.Second * 15,
+			Handler: func(sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+				buffAura.Activate(sim)
+			},
+		})
+	})
 
 	// https://www.wowhead.com/classic/item=234198/signet-ring-of-the-bronze-dragonflight
 	core.NewItemEffect(SignetRingBronzeConquerorR5, TimeswornStrikeAura)
@@ -102,7 +216,126 @@ func init() {
 	core.NewItemEffect(SignetRingBronzeSubjugatorR2, TimewornDecayAura)
 	core.NewItemEffect(SignetRingBronzeSubjugatorR1, TimewornDecayAura)
 
+	///////////////////////////////////////////////////////////////////////////
+	//                                 Other
+	///////////////////////////////////////////////////////////////////////////
+
+	// https://www.wowhead.com/classic/item=233505/leggings-of-immersion
+	// Your damaging non-periodic Nature spell critical strikes have a chance to grant you 40 increased spell damage for 10 sec.
+	// (Proc chance: 50%, 5s cooldown)
+	core.NewItemEffect(LeggingsOfImmersion, func(agent core.Agent) {
+		character := agent.GetCharacter()
+		buffAura := character.RegisterAura(core.Aura{
+			ActionID: core.ActionID{SpellID: 474126},
+			Label:    "Immersed",
+			Duration: time.Second * 10,
+			OnGain: func(aura *core.Aura, sim *core.Simulation) {
+				character.AddStatDynamic(sim, stats.SpellDamage, 40)
+			},
+			OnExpire: func(aura *core.Aura, sim *core.Simulation) {
+				character.AddStatDynamic(sim, stats.SpellDamage, -40)
+			},
+		})
+
+		icd := core.Cooldown{
+			Timer:    character.NewTimer(),
+			Duration: time.Second * 5,
+		}
+
+		core.MakeProcTriggerAura(&agent.GetCharacter().Unit, core.ProcTrigger{
+			Name:     "Immersed Trigger",
+			Callback: core.CallbackOnSpellHitDealt,
+			ProcMask: core.ProcMaskSpellDamage,
+			Handler: func(sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+				if spell.SpellSchool.Matches(core.SpellSchoolNature) && result.DidCrit() && icd.IsReady(sim) && sim.Proc(0.50, "Immersed Proc") {
+					buffAura.Activate(sim)
+					icd.Use(sim)
+				}
+			},
+		})
+	})
+
+	// https://www.wowhead.com/classic/item=233575/robes-of-the-battleguard
+	// Your damaging non-periodic spells increase your spell damage by 20 for 15 sec.
+	// If the target is player controlled, gain 120 spell penetration for 15 sec instead.
+	// (15s cooldown)
+	core.NewItemEffect(RobesOfTheBattleguard, func(agent core.Agent) {
+		character := agent.GetCharacter()
+		buffAura := character.RegisterAura(core.Aura{
+			ActionID: core.ActionID{SpellID: 1213241},
+			Label:    "Resolve of the Battleguard",
+			Duration: time.Second * 15,
+			OnGain: func(aura *core.Aura, sim *core.Simulation) {
+				character.AddStatDynamic(sim, stats.SpellDamage, 20)
+			},
+			OnExpire: func(aura *core.Aura, sim *core.Simulation) {
+				character.AddStatDynamic(sim, stats.SpellDamage, -20)
+			},
+		})
+
+		core.MakeProcTriggerAura(&agent.GetCharacter().Unit, core.ProcTrigger{
+			Name:       "Resolve of the Battleguard Trigger",
+			Callback:   core.CallbackOnSpellHitDealt,
+			Outcome:    core.OutcomeLanded,
+			ProcMask:   core.ProcMaskSpellDamage,
+			ProcChance: 1.00,
+			ICD:        time.Second * 15,
+			Handler: func(sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+				buffAura.Activate(sim)
+			},
+		})
+	})
+
 	core.AddEffectsToTest = true
+}
+
+// https://www.wowhead.com/classic/spell=1214125/obsidian-edged
+// Chance on direct spell damage or melee attack to deal 325 to 500 Fire damage to your target.
+// Deals extra damage to Silithid creatures in Silithus on almost every hit.
+// (2.1s cooldown)
+func ObsidianEdgedAura(itemID int32, agent core.Agent) {
+	character := agent.GetCharacter()
+
+	damageSpell := character.GetOrRegisterSpell(core.SpellConfig{
+		ActionID:    core.ActionID{SpellID: 1213941},
+		SpellSchool: core.SpellSchoolFire,
+		DefenseType: core.DefenseTypeMagic,
+		ProcMask:    core.ProcMaskSpellProc | core.ProcMaskSpellDamageProc,
+		Flags:       core.SpellFlagNoOnCastComplete | core.SpellFlagPassiveSpell,
+
+		DamageMultiplier: 1,
+		ThreatMultiplier: 1,
+
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+			spell.CalcAndDealDamage(sim, target, sim.Roll(325, 500), spell.OutcomeMagicHitAndCrit)
+		},
+	})
+
+	meleeProcMask := character.GetProcMaskForItem(itemID)
+
+	core.MakeProcTriggerAura(&character.Unit, core.ProcTrigger{
+		Name:     "Obsidian Edged Proc Melee",
+		Callback: core.CallbackOnSpellHitDealt,
+		Outcome:  core.OutcomeLanded,
+		ProcMask: meleeProcMask,
+		PPM:      1.0, // TODO: Determine PPM
+		ICD:      time.Millisecond * 2100,
+		Handler: func(sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+			damageSpell.Cast(sim, result.Target)
+		},
+	})
+
+	core.MakeProcTriggerAura(&character.Unit, core.ProcTrigger{
+		Name:       "Obsidian Edged Proc Melee",
+		Callback:   core.CallbackOnSpellHitDealt,
+		Outcome:    core.OutcomeLanded,
+		ProcMask:   core.ProcMaskSpellDamage,
+		ProcChance: 1.0, // Wowhead and Wago show 100% but this seems unlikely due to the tooltip. We'll see
+		ICD:        time.Millisecond * 2100,
+		Handler: func(sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+			damageSpell.Cast(sim, result.Target)
+		},
+	})
 }
 
 // https://www.wowhead.com/classic/spell=1214155/timeworn-decay
