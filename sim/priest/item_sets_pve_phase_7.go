730c3260314ea9a9b1be16c217efab5b0811c3e5
Kayla Glick <kaylaglick12@gmail.com>
2024-12-27 01:39:06 -0500

    paladin tier 3

--- sim/priest/item_sets_pve_phase_7.go
+++ sim/priest/item_sets_pve_phase_7.go
@@ -94,10 +94,14 @@ func (priest *Priest) applyNaxxramasShadow6PBonus() {
 
 				oldApplyEffects := spell.ApplyEffects
 				spell.ApplyEffects = func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-					critChance := priest.GetStat(stats.SpellCrit) / 100
-					spell.DamageMultiplierAdditive += critChance
+					critChanceBonus := 0.0
+					if target.MobType == proto.MobType_MobTypeUndead {
+						critChanceBonus = priest.GetStat(stats.SpellCrit) / 100
+					}
+
+					spell.DamageMultiplierAdditive += critChanceBonus
 					oldApplyEffects(sim, target, spell)
-					spell.DamageMultiplierAdditive -= critChance
+					spell.DamageMultiplierAdditive -= critChanceBonus
 				}
 			}
 		},
