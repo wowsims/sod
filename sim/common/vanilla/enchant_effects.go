535e7bea3597a7c7f28ef6e75222a6f96a4fe86d
Kayla Glick <kaylaglick12@gmail.com>
2024-12-29 17:12:48 -0500

    fix lifestealing

--- sim/common/vanilla/enchant_effects.go
+++ sim/common/vanilla/enchant_effects.go
@@ -273,7 +273,7 @@ func init() {
 			ThreatMultiplier: 1,
 
 			ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-				spell.CalcAndDealDamage(sim, target, 3, spell.OutcomeMagicHitAndCrit)
+				spell.CalcAndDealDamage(sim, target, 30, spell.OutcomeMagicHitAndCrit)
 			},
 		})
 
