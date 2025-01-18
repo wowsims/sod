60cc22637ecf55dd9ca1ffc5605dba738f7ad9ef
Nathan Berman <ncberman@asu.edu>
2025-01-18 17:45:03 +0000

    Made changes to how catlike static stats are applied to occur during build phase

--- sim/hunter/runes.go
+++ sim/hunter/runes.go
@@ -435,21 +435,47 @@ func (hunter *Hunter) applyHitAndRun() {
 }
 
 func (hunter *Hunter) applyCatlikeReflexes() {
-	if hunter.HasRune(proto.HunterRune_RuneHelmCatlikeReflexes) {
-		label := "Catlike Reflexes"
-		core.MakePermanent(hunter.RegisterAura(core.Aura{
-			Label: label,
-			OnInit: func(aura *core.Aura, sim *core.Simulation) {
-				hunter.AddStatDynamic(sim, stats.Dodge, 20*core.DodgeRatingPerDodgeChance)
+	if !hunter.HasRune(proto.HunterRune_RuneHelmCatlikeReflexes) {
+		return
+	}
+	label := "Catlike Reflexes"
+
+
+	core.MakePermanent(hunter.RegisterAura(core.Aura{
+		Label: label,
+		BuildPhase: core.CharacterBuildPhaseBuffs,
+		OnInit: func(aura *core.Aura, sim *core.Simulation) {
+			if hunter.FlankingStrike != nil {
+				hunter.FlankingStrike.CD.Multiplier -= 50
+			}
+		},
+		OnGain: func(aura *core.Aura, sim *core.Simulation) {
+			if aura.Unit.Env.MeasuringStats && aura.Unit.Env.State != core.Finalized {
+			  hunter.AddStat(stats.Dodge, 20*core.DodgeRatingPerDodgeChance)
 				if hunter.pet != nil {
 					hunter.pet.AddStat(stats.Dodge, 9*core.DodgeRatingPerDodgeChance)
 				}
-				if hunter.FlankingStrike != nil {
-					hunter.FlankingStrike.CD.Multiplier -= 50
+			} else {
+				hunter.AddStatDynamic(sim, stats.Dodge, 20*core.DodgeRatingPerDodgeChance)
+				if hunter.pet != nil {
+					hunter.pet.AddStatDynamic(sim, stats.Dodge, 9*core.DodgeRatingPerDodgeChance)
 				}
-			},
-		}))
-	}
+			}
+		},
+		OnExpire: func(aura *core.Aura, sim *core.Simulation) {
+			if aura.Unit.Env.MeasuringStats && aura.Unit.Env.State != core.Finalized {
+			  hunter.AddStat(stats.Dodge, -20*core.DodgeRatingPerDodgeChance)
+				if hunter.pet != nil {
+					hunter.pet.AddStat(stats.Dodge, -9*core.DodgeRatingPerDodgeChance)
+				}
+			} else {
+				hunter.AddStatDynamic(sim, stats.Dodge, -20*core.DodgeRatingPerDodgeChance)
+				if hunter.pet != nil {
+					hunter.pet.AddStatDynamic(sim, stats.Dodge, -9*core.DodgeRatingPerDodgeChance)
+				}
+			}
+		},
+	}))
 }
 
 func (hunter *Hunter) applyImprovedVolley() {
