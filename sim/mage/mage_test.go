07684f412643aa98a61ac3c4639293df7a783ada
Kayla Glick <kaylaglick12@gmail.com>
2025-03-17 01:42:00 -0400

    remove phase 6 test setup

--- sim/mage/mage_test.go
+++ sim/mage/mage_test.go
@@ -259,9 +259,6 @@ var Phase5TalentsArcane = "2500550010031531--2035020310004"
 var Phase5TalentsFire = "21-5052300123033151-203500031"
 var phase5talentsfrost = "005005001--20353203112351351"
 
-var Phase6TalentsFire = "-0552323121033151-203500031"
-var phase6talentsfrost = "005005001--20353203112351351"
-
 var PlayerOptionsArcane = &proto.Player_Mage{
 	Mage: &proto.Mage{
 		Options: &proto.Mage_Options{
@@ -343,19 +340,6 @@ var Phase5Consumes = core.ConsumesCombo{
 		Food:           proto.Food_FoodRunnTumTuberSurprise,
 		MainHandImbue:  proto.WeaponImbue_BrilliantWizardOil,
 		SpellPowerBuff: proto.SpellPowerBuff_GreaterArcaneElixir,
-	},
-}
-
-var Phase6Consumes = core.ConsumesCombo{
-	Label: "P6-Consumes",
-	Consumes: &proto.Consumes{
-		DefaultPotion:  proto.Potions_MajorManaPotion,
-		Flask:          proto.Flask_FlaskOfAncientKnowledge,
-		FirePowerBuff:  proto.FirePowerBuff_ElixirOfGreaterFirepower,
-		FrostPowerBuff: proto.FrostPowerBuff_ElixirOfFrostPower,
-		Food:           proto.Food_FoodDarkclawBisque,
-		MainHandImbue:  proto.WeaponImbue_EnchantedRepellent,
-		SpellPowerBuff: proto.SpellPowerBuff_ElixirOfTheMageLord,
 	},
 }
 
