50424b1f9ccc9969ff82b52126ad3a2efe054f58
Kayla Glick <kaylaglick12@gmail.com>
2025-03-27 20:04:05 -0400

    update mage T4

--- sim/mage/item_sets_pve_phase_8.go
+++ sim/mage/item_sets_pve_phase_8.go
@@ -73,8 +73,8 @@ func (mage *Mage) applyScarletEnclaveDamage2PBonus() {
 	}
 }
 
-// Casts of Pyroblast remove a stack of Balefire Bolt.
-// Casts of Deep Freeze increase the duration of your active Icy Veins by 8 seconds.
+// Casting Deep Freeze increases the remaining duration of your Icy Veins spell by 8 sec.
+// Casting Pyroblast cancels 1 stack of the effect from your Balefire Bolt.
 func (mage *Mage) applyScarletEnclaveDamage4PBonus() {
 	label := "S03 - Item - Scarlet Enclave - Mage - Damage 4P Bonus"
 	if mage.HasAura(label) {
@@ -102,9 +102,10 @@ func (mage *Mage) applyScarletEnclaveDamage4PBonus() {
 	}
 }
 
-// Fire Blast cooldown is reduced by 5 seconds and refreshes Living Bomb.
-// Frozen Orb cooldown is reduced by 25 seconds.
-// When you use consume stacks of Glaciate the cooldown of Deep Freeze is reduced by 1 second per stack consumed.
+// Reduces the cooldown on your Frozen Orb spell by 20 sec.
+// Each time Glaciate is consumed, the cooldown on your Deep Freeze is reduced by 1.0 sec per stack consumed.
+// Increases the chance for Arcane Blast to trigger Missile Barrage by 10% and for Fireball and Frostbolt to trigger Missile Barrage by 5%.
+// Reduces the cooldown on Fire Blast by 5 sec and Fire Blast now refreshes the duration of your Living Bomb on the target.
 func (mage *Mage) applyScarletEnclaveDamage6PBonus() {
 	label := "S03 - Item - Scarlet Enclave - Mage - Damage 6P Bonus"
 	if mage.HasAura(label) {
@@ -113,25 +114,13 @@ func (mage *Mage) applyScarletEnclaveDamage6PBonus() {
 
 	aura := core.MakePermanent(mage.RegisterAura(core.Aura{
 		Label: label,
-	})).AttachSpellMod(core.SpellModConfig{
-		Kind:      core.SpellMod_Cooldown_Flat,
-		ClassMask: ClassSpellMask_MageFireBlast,
-		TimeValue: -time.Second * 5,
-	})
-
-	if mage.HasRune(proto.MageRune_RuneHandsLivingBomb) {
-		aura.OnSpellHitDealt = func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
-			if dot := mage.LivingBomb.Dot(result.Target); dot.IsActive() && spell.Matches(ClassSpellMask_MageFireBlast) && result.Landed() {
-				dot.Refresh(sim)
-			}
-		}
-	}
+	}))
 
 	if mage.HasRune(proto.MageRune_RuneCloakFrozenOrb) {
 		aura.AttachSpellMod(core.SpellModConfig{
 			Kind:      core.SpellMod_Cooldown_Flat,
 			ClassMask: ClassSpellMask_MageFrozenOrb,
-			TimeValue: -time.Second * 25,
+			TimeValue: -time.Second * 20,
 		})
 	}
 
@@ -149,6 +138,27 @@ func (mage *Mage) applyScarletEnclaveDamage6PBonus() {
 				})
 			}
 		})
+	}
+
+	if mage.HasRune(proto.MageRune_RuneBeltMissileBarrage) {
+		if mage.HasRune(proto.MageRune_RuneHandsArcaneBlast) {
+			mage.ArcaneBlastMissileBarrageChance += 0.10
+			mage.FireballFrostboltMissileBarrageChance += 0.05
+		}
+	}
+
+	aura.AttachSpellMod(core.SpellModConfig{
+		Kind:      core.SpellMod_Cooldown_Flat,
+		ClassMask: ClassSpellMask_MageFireBlast,
+		TimeValue: -time.Second * 5,
+	})
+
+	if mage.HasRune(proto.MageRune_RuneHandsLivingBomb) {
+		aura.OnSpellHitDealt = func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+			if dot := mage.LivingBomb.Dot(result.Target); dot.IsActive() && spell.Matches(ClassSpellMask_MageFireBlast) && result.Landed() {
+				dot.Refresh(sim)
+			}
+		}
 	}
 }
 
