bbfb6222e7ec457d73c5ad0c6844c9b0c613fabd
Fynnen <lapierre.francis@gmail.com>
2025-03-28 00:10:58 -0400

    Draft implementation of P8 Rogue Tank 6P Set Bonus

---
+++ sim/rogue/item_sets_phase_8.go
@@ -0,0 +1,112 @@
+package rogue
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/wowsims/sod/sim/core"
+	"github.com/wowsims/sod/sim/core/proto"
+	"github.com/wowsims/sod/sim/core/stats"
+)
+
+var ItemSetDuskwraithArmor = core.NewItemSet(core.ItemSet{
+	Name: "Duskwraith Armor",
+	Bonuses: map[int32]core.ApplyEffect{
+		2: func(agent core.Agent) {
+			rogue := agent.(RogueAgent).GetRogue()
+			rogue.applyScarletEnclaveDamage2PBonus()
+		},
+		4: func(agent core.Agent) {
+			rogue := agent.(RogueAgent).GetRogue()
+			rogue.applyScarletEnclaveDamage4PBonus()
+		},
+		6: func(agent core.Agent) {
+			rogue := agent.(RogueAgent).GetRogue()
+			rogue.applyScarletEnclaveDamage6PBonus()
+		},
+	},
+})
+
+var ItemSetDuskwraithLeathers = core.NewItemSet(core.ItemSet{
+	Name: "Duskwraith Leathers",
+	Bonuses: map[int32]core.ApplyEffect{
+		2: func(agent core.Agent) {
+			rogue := agent.(RogueAgent).GetRogue()
+			rogue.applyScarletEnclaveTank2PBonus()
+		},
+		4: func(agent core.Agent) {
+			rogue := agent.(RogueAgent).GetRogue()
+			rogue.applyScarletEnclaveTank4PBonus()
+		},
+		6: func(agent core.Agent) {
+			rogue := agent.(RogueAgent).GetRogue()
+			rogue.applyScarletEnclaveTank6PBonus()
+		},
+	}
+})
+
+// Your stacks of Rolling with the Punches also increase all damage you deal by 1%.
+func (rogue *Rogue) applyScarletEnclaveTank2PBonus() {
+	label := "S03 - Item - Scarlet Enclave - Rogue - Tank 2P Bonus"
+}
+
+// Your Blade Flurry now also strikes a third target and increases your attack speed by an additional 10%. In addition, each combo point you spend reduces the remaining cooldown on your Blade Flurry by 0.5 sec.
+func (rogue *Rogue) applyScarletEnclaveTank4PBonus() {
+	label := "S03 - Item - Scarlet Enclave - Rogue - Tank 4P Bonus"
+
+	if rogue.HasAura(label) {
+		return
+	}
+
+}
+
+// Your Rolling with the Punches can now stack up to 10 times, but grants 2% less health per stack. At 10 stacks, each time you Dodge you will gain 15 Energy.
+func (rogue *Rogue) applyScarletEnclaveTank6PBonus() {
+	label := "S03 - Item - Scarlet Enclave - Rogue - Tank 6P Bonus"
+	
+	statDeps := make([]*stats.StatDependency, 11) // 10 stacks + zero condition
+	for i := 1; i < 6; i++ {
+		statDeps[i] = rogue.NewDynamicMultiplyStat(stats.Health, 1.0+.04*float64(i)) // 4% health per stack
+
+	}
+	metrics := rogue.NewEnergyMetrics(core.ActionID{SpellID: 1226956})
+
+	energyProc := rogue.RegisterSpell(core.SpellConfig{
+		ActionID:    core.ActionID{SpellID: 1226956},
+		SpellSchool: core.SpellSchoolNature,
+		ApplyEffects: func(sim *core.Simulation, u *core.Unit, spell *core.Spell) {
+			rogue.AddEnergy(sim, 15, metrics)
+		},
+	})
+
+	energyAura := rogue.RegisterAura(core.Aura{
+		ActionID:  core.ActionID{SpellID: 1219291},
+		Label:     "S03 - Item - Scarlet Enclave - Rogue - Tank 6P Bonus Energy Gain",
+		OnSpellHitTaken: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+			if result.Outcome.Matches(core.OutcomeDodge) {
+				energyProc.Cast(sim, result.Target)
+			}
+		},
+	})
+
+	core.MakePermanent(rogue.RegisterAura(core.Aura{
+		Label: label,
+		OnInit: func(aura *core.aura, sim *core.Simulation) {
+			rogue.RollingWithThePunchesProcAura.maxStacks += 5
+			rogue.RollingWithThePunchesProcAura.OnStacksChange = func(aura *core.Aura, sim *core.Simulation, oldStacks int32, newStacks int32) {
+				if oldStacks != 0 {
+					aura.Unit.DisableDynamicStatDep(sim, statDeps[oldStacks])
+				}
+				if newStacks != 0 {
+					aura.Unit.EnableDynamicStatDep(sim, statDeps[newStacks])
+				}
+				if newStacks == 10 {
+					energyAura.Activate(sim)
+				}
+				if newStacks != 10 & oldStacks == 10 {
+					energyAura.Deactivate(sim)
+				}
+			}
+		},
+	}))
+}
