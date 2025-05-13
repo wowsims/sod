02f56db974f15c76cfd98869660c30a6598802e5
Kayla Glick <kaylaglick12@gmail.com>
2025-05-12 22:35:06 -0400

    remove old SBV damage multiplier code

--- sim/warlock/shadowbolt.go
+++ sim/warlock/shadowbolt.go
@@ -4,22 +4,17 @@ import (
 	"time"
 
 	"github.com/wowsims/sod/sim/core"
-	"github.com/wowsims/sod/sim/core/proto"
 )
 
 const ShadowBoltRanks = 10
 
 func (warlock *Warlock) getShadowBoltBaseConfig(rank int) core.SpellConfig {
-	hasShadowBoltVolleyRune := warlock.HasRune(proto.WarlockRune_RuneHandsShadowBoltVolley)
-
 	spellCoeff := [ShadowBoltRanks + 1]float64{0, .14, .299, .56, .857, .857, .857, .857, .857, .857, .857}[rank]
 	baseDamage := [ShadowBoltRanks + 1][]float64{{0}, {13, 18}, {26, 32}, {52, 61}, {92, 104}, {150, 170}, {213, 240}, {292, 327}, {373, 415}, {455, 507}, {482, 538}}[rank]
 	spellId := [ShadowBoltRanks + 1]int32{0, 686, 695, 705, 1088, 1106, 7641, 11659, 11660, 11661, 25307}[rank]
 	manaCost := [ShadowBoltRanks + 1]float64{0, 25, 40, 70, 110, 160, 210, 265, 315, 370, 380}[rank]
 	level := [ShadowBoltRanks + 1]int{0, 1, 6, 12, 20, 28, 36, 44, 52, 60, 60}[rank]
 	castTime := [ShadowBoltRanks + 1]int32{0, 1700, 2200, 2800, 3000, 3000, 3000, 3000, 3000, 3000, 3000}[rank]
-
-	damageMulti := core.TernaryFloat64(hasShadowBoltVolleyRune && warlock.Env.GetNumTargets() > 1, 0.70, 1.0)
 
 	return core.SpellConfig{
 		ClassSpellMask: ClassSpellMask_WarlockShadowBolt,
@@ -46,7 +41,7 @@ func (warlock *Warlock) getShadowBoltBaseConfig(rank int) core.SpellConfig {
 			return warlock.MetamorphosisAura == nil || !warlock.MetamorphosisAura.IsActive()
 		},
 
-		DamageMultiplier: damageMulti,
+		DamageMultiplier: 1.0,
 		ThreatMultiplier: 1,
 		BonusCoefficient: spellCoeff,
 
